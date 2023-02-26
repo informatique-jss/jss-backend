1a3fa10a01fa8ae9f249182ef9eddfe057a4aad4
Alex Gapin <agapin@jss.fr>
2023-02-26 13:15:09 +0100

    feature/OSI-930

--- src/main/java/com/jss/osiris/modules/invoicing/service/PaymentServiceImpl.java
+++ src/main/java/com/jss/osiris/modules/invoicing/service/PaymentServiceImpl.java
@@ -750,7 +750,7 @@ public class PaymentServiceImpl implements PaymentService {
 
                 if (idToFind != null) {
                     tmpEntitiesFound = searchService.searchForEntitiesById(idToFind, entityTypesToSearch);
-                    debourFound = searchService.searchForEntities(idToFind + "", Debour.class.getSimpleName());
+                    debourFound = searchService.searchForEntities(idToFind + "", Debour.class.getSimpleName(), true);
                     if (debourFound != null && debourFound.size() > 0)
                         tmpEntitiesFound.addAll(debourFound);
                 }
