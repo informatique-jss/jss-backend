83fb971368d48f5f95adcd90a9908d88f04fd241
Alex Gapin <agapin@jss.fr>
2023-05-12 00:56:35 +0200

    fix

--- src/main/java/com/jss/osiris/modules/tiers/repository/TiersRepository.java
+++ src/main/java/com/jss/osiris/modules/tiers/repository/TiersRepository.java
@@ -20,21 +20,6 @@ public interface TiersRepository extends QueryCacheCrudRepository<Tiers, Integer
 
         List<Tiers> findByTiersType(TiersType tiersTypeClient);
 
-        @Query(nativeQuery = true, value = "select distinct t.* " +
-                        " from tiers t " +
-                        " left join responsable r on r.id_tiers = t.id " +
-                        " left join invoice i1 on i1.id_tiers = t.id and i1.id_invoice_status = :invoiceStatusSendId " +
-                        " left join invoice i2 on i2.id_responsable = r.id and i2.id_invoice_status = :invoiceStatusSendId "
-                        +
-                        " left join customer_order c1 on c1.id_tiers = t.id  " +
-                        " left join customer_order c2 on c2.id_responsable = t.id  " +
-                        " left join deposit d1 on d1.id_customer_order = c1.id and d1.is_cancelled = false " +
-                        " left join deposit d2 on d2.id_customer_order = c2.id and d2.is_cancelled = false  " +
-                        " where t.id_tiers_type = :tiersTypeClientId " +
-                        " and coalesce(i1.id, i2.id, d1.id, d2.id) is not null ")
-        List<Tiers> findAllTiersForBillingClosureReceiptSend(@Param("invoiceStatusSendId") Integer invoiceStatusSendId,
-                        @Param("tiersTypeClientId") Integer tiersTypeClientId);
-
     @Query(nativeQuery = true, value = "select distinct t.* " +
             " from tiers t " +
             " left join responsable r on r.id_tiers = t.id " +
