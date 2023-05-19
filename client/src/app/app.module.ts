0168b208e3c21d7bfd3f8a1128699be2cee87008
ESTEVES Miguel <mesteves@jss.fr>
2023-05-19 17:41:21 +0200

    github ajustement v3

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { TiersModule } from './modules/tiers/components/tiers/tiers.module';
 import { HeaderComponent } from './routing/header/header.component';
 import { RoutingModule } from './routing/routing.module';
 import { SidenavListComponent } from './routing/sidenav-list/sidenav-list.component';
+
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
