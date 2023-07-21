cd60aee23694f35145d97790ca9b6fa87e64a256
ESTEVES Miguel <mesteves@jss.fr>
2023-07-21 12:33:46 +0200

    Added formJuridique from Rne and merge with develop

--- src/main/java/com/jss/osiris/libs/PrintDelegate.java
+++ src/main/java/com/jss/osiris/libs/PrintDelegate.java
@@ -12,15 +12,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.jss.osiris.libs.exception.OsirisClientMessageException;
 import com.jss.osiris.libs.exception.OsirisException;
 import com.jss.osiris.modules.invoicing.model.InvoiceLabelResult;
 import com.jss.osiris.modules.miscellaneous.service.ConstantService;
 import com.jss.osiris.modules.quotation.model.Affaire;
 import com.jss.osiris.modules.quotation.model.CustomerOrder;
 import com.jss.osiris.modules.quotation.model.Provision;
+import com.jss.osiris.modules.quotation.model.ProvisionType;
+import com.jss.osiris.modules.quotation.model.guichetUnique.RneCompany;
+import com.jss.osiris.modules.quotation.service.RneDelegateService;
 
 @Service
 public class PrintDelegate {
+
+  @Autowired
+  RneDelegateService rneDelegateService;
 
   @Value("${printer.label.ip}")
   private String printerIp;
@@ -31,41 +38,40 @@ public class PrintDelegate {
   @Autowired
   ConstantService constantService;
 
-  public void printMailingLabel(InvoiceLabelResult label, CustomerOrder customerOrder, boolean printProvisionRegister)
-      throws OsirisException {
+  String provisionType, registreAC, affaireName, affaireAddress, cpCedex, siren, legalForm = "";
 
-    String provisionType = "";
-    String registreAC = "";
-    String nomAffaire = "";
-    String addressAffaire = "";
-    String cpCedexVille = "";
-    String siren = "";
-    String formeJuridiqueAffaire = "";
+  public void printMailingLabel(InvoiceLabelResult label, CustomerOrder customerOrder, boolean printProvisionRegister)
+      throws OsirisException, OsirisClientMessageException {
 
     if (customerOrder.getAssoAffaireOrders() != null
         && !customerOrder.getAssoAffaireOrders().isEmpty()
         && customerOrder.getAssoAffaireOrders().get(0).getAffaire() != null) {
-      Affaire affaire = customerOrder.getAssoAffaireOrders().get(0).getAffaire();
 
-      if (affaire.getDenomination() != null) {
-        nomAffaire = affaire.getDenomination();
-      }
+      Affaire affaire = customerOrder.getAssoAffaireOrders().get(0).getAffaire();
+      RneCompany rneCompany = rneDelegateService.getCompanyBySiren(affaire.getSiren());
+      legalForm = legalForm = rneCompany.getFormality().getFormeJuridique();
+
+      if (affaire.getDenomination() != null)
+        affaireName = affaire.getDenomination();
 
       if (affaire.getAddress() != null) {
-        addressAffaire = affaire.getAddress();
+        affaireAddress = affaire.getAddress();
+        if (affaire.getCity() != null) {
+          affaireAddress += "-" + affaire.getCity().getLabel();
+        }
+        if (affaire.getPostalCode() != null) {
+          affaireAddress += "-" + affaire.getPostalCode();
+        }
       }
+      if (affaire.getCedexComplement() != null)
+        cpCedex = affaire.getCedexComplement();
 
-      if (affaire.getCedexComplement() != null) {
-        cpCedexVille = affaire.getCedexComplement();
-      }
-
-      if (affaire.getSiren() != null) {
+      if (affaire.getSiren() != null)
         siren = affaire.getSiren();
-      }
 
-      if (affaire.getFormeJuridique() != null) {
-        formeJuridiqueAffaire = affaire.getFormeJuridique().getLabel();
-      }
+      if (legalForm != null)
+        legalForm = rneCompany.getFormality().getFormeJuridique();
+
     }
 
     boolean hasProvisionRegister = false;
@@ -95,63 +101,64 @@ public class PrintDelegate {
 
       if (hasProvisionRegister && printProvisionRegister) {
 
-        dOut.writeUTF("\r\n");
-        if (provisionType != null)
-          dOut.writeUTF("   " + StringUtils.stripAccents(provisionType.toUpperCase()));
-        dOut.writeUTF("\r\n");
-
-        dOut.flush();
-        if (registreAC != null)
-          dOut.writeUTF("         " + StringUtils.stripAccents(registreAC.toUpperCase()));
+        dOut.writeUTF(" " + customerOrder.getId());
         dOut.writeUTF("\r\n");
         dOut.flush();
-        if (nomAffaire != null)
-          dOut.writeUTF("          " + StringUtils.stripAccents(nomAffaire.toUpperCase()));
+        dOut.writeUTF(
+            "       " + StringUtils
+                .stripAccents((provisionType != null ? provisionType
+                    : ""))
+                .toUpperCase());
         dOut.writeUTF("\r\n");
         dOut.flush();
-        if (formeJuridiqueAffaire != null) {
-          String upperCaseFormeJuridique = StringUtils.stripAccents(formeJuridiqueAffaire.toUpperCase());
-          int maxLengthPerLine = 55;
-
-          StringBuilder wrappedString = new StringBuilder();
-          int length = upperCaseFormeJuridique.length();
-          int startIndex = 0;
-
-          while (startIndex < length) {
-            int endIndex = Math.min(startIndex + maxLengthPerLine, length);
-            String line = upperCaseFormeJuridique.substring(startIndex, endIndex);
-
-            if (endIndex < length && line.charAt(maxLengthPerLine - 1) != ' '
-                && upperCaseFormeJuridique.charAt(endIndex) != ' ') {
-
-              int lastSpaceIndex = line.lastIndexOf(" ");
-              if (lastSpaceIndex != -1) {
-                endIndex = startIndex + lastSpaceIndex + 1;
-                line = upperCaseFormeJuridique.substring(startIndex, endIndex);
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "               " + StringUtils
+                .stripAccents((affaireName != null ? affaireName
+                    : ""))
+                .toUpperCase());
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        if (affaireAddress != null) {
+          List<String> labelLines = Arrays.asList(affaireAddress.split("\\R"));
+          if (labelLines != null) {
+            for (String line : labelLines) {
+              List<String> lineToPrint = new ArrayList<String>();
+              if (line.contains("\r\n")) {
+                lineToPrint = Arrays.asList(line.split("\r\n"));
+              } else
+                lineToPrint.add(line);
+              for (String lin : lineToPrint) {
+                dOut.writeUTF("              " + StringUtils.stripAccents(lin).toUpperCase());
+                dOut.writeUTF("\r\n");
+                dOut.flush();
               }
             }
-
-            wrappedString.append("         ").append(line).append(System.lineSeparator());
-            startIndex = endIndex;
           }
-
-          dOut.writeUTF(wrappedString.toString());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-        }
-
-        if (addressAffaire != null) {
-          dOut.writeUTF("          " + StringUtils.stripAccents(addressAffaire));
-          dOut.flush();
         }
         dOut.writeUTF("\r\n");
-        if (cpCedexVille != null) {
-          dOut.writeUTF("          " + cpCedexVille);
-        }
+        dOut.flush();
+        dOut.writeUTF("               " + StringUtils.stripAccents((cpCedex != null
+            ? cpCedex
+            : ""))
+            .toUpperCase() + " ");
         dOut.writeUTF("\r\n");
-        if (siren != null) {
-          dOut.writeUTF("          " + siren);
-        }
+        dOut.flush();
+        dOut.writeUTF(
+            "               " + StringUtils
+                .stripAccents((siren != null ? siren
+                    : ""))
+                .toUpperCase());
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "               " + StringUtils
+                .stripAccents((legalForm != null ? legalForm
+                    : ""))
+                .toUpperCase());
       } else {
 
         // Handle manual adresses
