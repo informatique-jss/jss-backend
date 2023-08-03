a37afbd1a7986b759ae169975addd4b1384932ac
Alex Gapin <agapin@jss.fr>
2023-08-03 11:31:25 +0200

    feature/OSI-2785

--- src/main/java/com/jss/osiris/modules/quotation/model/Provision.java
+++ src/main/java/com/jss/osiris/modules/quotation/model/Provision.java
@@ -67,6 +67,7 @@ public class Provision implements IId, IAttachment {
 	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JsonIgnoreProperties(value = { "provision" }, allowSetters = true)
 	@JoinColumn(name = "id_formalite")
+	@IndexedField
 	private Formalite formalite;
 
 	@OneToMany(targetEntity = InvoiceItem.class, mappedBy = "provision", cascade = CascadeType.REMOVE)
