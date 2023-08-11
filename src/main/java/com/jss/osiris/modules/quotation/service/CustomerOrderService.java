c237e401f5915c2ba324bceecd9d699a5f93a2b7
ESTEVES Miguel <mesteves@jss.fr>
2023-08-11 17:21:34 +0200

    remove import

--- src/main/java/com/jss/osiris/modules/quotation/service/CustomerOrderService.java
+++ src/main/java/com/jss/osiris/modules/quotation/service/CustomerOrderService.java
@@ -3,7 +3,6 @@ package com.jss.osiris.modules.quotation.service;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.jss.osiris.libs.exception.OsirisClientMessageException;
 import com.jss.osiris.libs.exception.OsirisException;
