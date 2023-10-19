1047290034bd9851bba6a25747a5bae12d409c4d
Alex Gapin <agapin@jss.fr>
2023-10-19 17:46:16 +0200

    fix

--- client/src/app/modules/quotation/components/quotation/quotation.component.ts
+++ client/src/app/modules/quotation/components/quotation/quotation.component.ts
@@ -8,7 +8,6 @@ import { CUSTOMER_ORDER_STATUS_BEING_PROCESSED, CUSTOMER_ORDER_STATUS_BILLED, CU
 import { getDocument } from 'src/app/libs/DocumentHelper';
 import { instanceOfCustomerOrder } from 'src/app/libs/TypeHelper';
 import { AssociatePaymentDialogComponent } from 'src/app/modules/invoicing/components/associate-payment-dialog/associate-payment-dialog.component';
-import { Vat } from 'src/app/modules/miscellaneous/model/Vat';
 import { ConstantService } from 'src/app/modules/miscellaneous/services/constant.service';
 import { Employee } from 'src/app/modules/profile/model/Employee';
 import { BillingLabelType } from 'src/app/modules/tiers/model/BillingLabelType';
@@ -667,10 +666,10 @@ export class QuotationComponent implements OnInit, AfterContentChecked {
   }
 
   getApplicableVat(): VatBase[] {
-    return QuotationComponent.computeApplicableVat(this.quotation, this.constantService.getVatDeductible());
+    return QuotationComponent.computeApplicableVat(this.quotation);
   }
 
-  public static computeApplicableVat(quotation: IQuotation, debourVat: Vat): VatBase[] {
+  public static computeApplicableVat(quotation: IQuotation): VatBase[] {
     let vatBases: VatBase[] = [];
     if (quotation && quotation.assoAffaireOrders) {
       for (let asso of quotation.assoAffaireOrders) {
@@ -678,7 +677,7 @@ export class QuotationComponent implements OnInit, AfterContentChecked {
           for (let provision of asso.provisions) {
             if (provision.invoiceItems) {
               for (let invoiceItem of provision.invoiceItems) {
-                if (invoiceItem.vat && invoiceItem.vatPrice && invoiceItem.vatPrice > 0) {
+                if (invoiceItem.vat && invoiceItem.vatPrice) {
                   let vatFound = false;
                   for (let vatBase of vatBases) {
                     if (vatBase.label == invoiceItem.vat.label) {
