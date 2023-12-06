5754b16bb96cb252eab522fd31b2dbb5dce38caf
ESTEVES Miguel <mesteves@jss.fr>
2023-12-06 17:49:29 +0100

    merge dev

--- src/main/java/com/jss/osiris/libs/PrintDelegate.java
+++ src/main/java/com/jss/osiris/libs/PrintDelegate.java
@@ -12,11 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.jss.osiris.libs.exception.OsirisClientMessageException;
 import com.jss.osiris.libs.exception.OsirisException;
 import com.jss.osiris.modules.invoicing.model.InvoiceLabelResult;
 import com.jss.osiris.modules.miscellaneous.model.City;
 import com.jss.osiris.modules.miscellaneous.model.CompetentAuthority;
+import com.jss.osiris.modules.miscellaneous.model.CompetentAuthorityType;
 import com.jss.osiris.modules.miscellaneous.repository.CityRepository;
 import com.jss.osiris.modules.miscellaneous.repository.CompetentAuthorityRepository;
 import com.jss.osiris.modules.miscellaneous.service.ConstantService;
@@ -30,7 +30,6 @@ import com.jss.osiris.modules.quotation.service.RneDelegateService;
 @Service
 public class PrintDelegate {
 
-<<<<<<< HEAD
   @Autowired
   RneDelegateService rneDelegateService;
 
@@ -40,12 +39,6 @@ public class PrintDelegate {
   @Autowired
   CompetentAuthorityRepository competentAuthorityRepository;
 
-  @Value("${printer.label.ip}")
-  private String printerIp;
-
-  @Value("${printer.label.port}")
-  private Integer printerPort;
-
   @Autowired
   ConstantService constantService;
 
@@ -54,8 +47,13 @@ public class PrintDelegate {
 
   String provisionType, registreAC, affaireName, affaireAddress, cpCedex, siren, legalForm = "";
 
-  public void printMailingLabel(InvoiceLabelResult label, CustomerOrder customerOrder, boolean printProvisionRegister)
-      throws OsirisException, OsirisClientMessageException {
+  @Value("${printer.label.ip}")
+  private String printerIp;
+
+  @Value("${printer.label.port}")
+  private Integer printerPort;
+
+  public void printMailingLabel(InvoiceLabelResult label, CustomerOrder customerOrder, boolean printProvisionRegister) throws OsirisException {
 
     if (customerOrder.getAssoAffaireOrders() != null
         && !customerOrder.getAssoAffaireOrders().isEmpty()
@@ -64,12 +62,13 @@ public class PrintDelegate {
       provisionType = customerOrder.getAssoAffaireOrders().get(0).getProvisions().get(0).getProvisionFamilyType()
           .getLabel();
       Affaire affaire = customerOrder.getAssoAffaireOrders().get(0).getAffaire();
-      RneCompany rneCompany = (affaire != null && affaire.getSiren() != null)
+      
+      List<RneCompany> rneCompany = (affaire != null && affaire.getSiren() != null)
           ? rneDelegateService.getCompanyBySiren(affaire.getSiren())
           : null;
 
-      String legalFormNumber = (rneCompany != null && rneCompany.getFormality() != null)
-          ? rneCompany.getFormality().getFormeJuridique()
+      String legalFormNumber = (rneCompany != null && rneCompany.get(0).getFormality() != null)
+          ? rneCompany.get(0).getFormality().getFormeJuridique().getLabel()
           : "";
       FormeJuridique legalForm = (legalFormNumber.isEmpty()) ? new FormeJuridique()
           : formeJuridiqueRepository.findByCode(legalFormNumber);
@@ -100,255 +99,101 @@ public class PrintDelegate {
 
       String cityRne = "";
       if (rneCompany != null &&
-          rneCompany.getFormality() != null &&
-          rneCompany.getFormality().getContent() != null &&
-          rneCompany.getFormality().getContent().getPersonneMorale() != null &&
-          rneCompany.getFormality().getContent().getPersonneMorale().getAdresseEntreprise() != null &&
-          rneCompany.getFormality().getContent().getPersonneMorale().getAdresseEntreprise()
+          rneCompany.get(0).getFormality() != null &&
+          rneCompany.get(0).getFormality().getContent() != null &&
+          rneCompany.get(0).getFormality().getContent().getPersonneMorale() != null &&
+          rneCompany.get(0).getFormality().getContent().getPersonneMorale().getAdresseEntreprise() != null &&
+          rneCompany.get(0).getFormality().getContent().getPersonneMorale().getAdresseEntreprise()
               .getAdresse() != null) {
-        cityRne = rneCompany.getFormality().getContent().getPersonneMorale().getAdresseEntreprise().getAdresse()
+        cityRne = rneCompany.get(0).getFormality().getContent().getPersonneMorale().getAdresseEntreprise().getAdresse()
             .getCommune();
       }
       List<City> city = cityRepository.findByLabelContainingIgnoreCase(cityRne);
       Integer idCity = (city != null && !city.isEmpty()) ? city.get(0).getId() : 0;
 
-      String competentAuthorityTypeRcsCode = (constantService.getCompetentAuthorityTypeRcs() != null)
-          ? constantService.getCompetentAuthorityTypeRcs().getCode()
-          : "";
-      CompetentAuthority ca = competentAuthorityRepository.findByCityIdAndByAuthorityType(idCity,
-          competentAuthorityTypeRcsCode);
+      CompetentAuthorityType competentAuthorityType = (constantService.getCompetentAuthorityTypeRcs() != null)
+          ? constantService.getCompetentAuthorityTypeRcs() : "";
+      List<CompetentAuthority> ca = competentAuthorityRepository.findByCityAndCompetentAuthorityType(city.get(0),
+          competentAuthorityType);
 
       if (ca != null) {
-        registreAC = ca.getLabel();
+        registreAC = ca.get(0).getLabel();
       } else {
         registreAC = "";
       }
-      Socket socket = null;
-      DataOutputStream dOut = null;
-
-      try {
-        socket = new Socket(printerIp, printerPort);
-        dOut = new DataOutputStream(socket.getOutputStream());
-
-        if (printProvisionRegister) {
-
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF(
-              "       " + StringUtils
-                  .stripAccents((provisionType != null ? provisionType
-                      : ""))
-                  .toUpperCase());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF(
-              "            " + StringUtils
-                  .stripAccents((affaireName != null ? affaireName
-                      : ""))
-                  .toUpperCase());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          if (affaireAddress != null) {
-            List<String> labelLines = Arrays.asList(affaireAddress.split("\\R"));
-            if (labelLines != null) {
-              for (String line : labelLines) {
-                List<String> lineToPrint = new ArrayList<String>();
-                if (line.contains("\r\n")) {
-                  lineToPrint = Arrays.asList(line.split("\r\n"));
-                } else
-                  lineToPrint.add(line);
-                for (String lin : lineToPrint) {
-                  dOut.writeUTF("            " + StringUtils.stripAccents(lin).toUpperCase());
-                  dOut.writeUTF("\r\n");
-                  dOut.flush();
-                }
-              }
-            }
-          }
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("             " + StringUtils.stripAccents((cpCedex != null
-              ? cpCedex
-              : ""))
-              .toUpperCase() + " ");
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF(
-              "             " + StringUtils
-                  .stripAccents((siren != null ? siren
-                      : ""))
-                  .toUpperCase());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF(
-              "            " + StringUtils.stripAccents((legalFormLabel != null ? legalFormLabel
-                  : ""))
-                  .toUpperCase());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF(
-              "            " + StringUtils.stripAccents((registreAC != null ? registreAC
-                  : ""))
-                  .toUpperCase());
-        } else {
-
-<<<<<<< HEAD
-          // Handle manual adresses
-          if (label.getBillingLabelCity() == null) {
-            List<String> lines = Arrays.asList(label.getBillingLabelAddress().split("\\R"));
-            if (lines != null && lines.size() > 1) {
-              label.setBillingLabelAddress(lines.get(0));
-              label.setBillingLabelPostalCode("");
-              for (int i = 1; i < lines.size(); i++)
-                label.setBillingLabelPostalCode(label.getBillingLabelPostalCode() + " " + lines.get(i));
-            }
-          }
-          dOut.writeUTF("\r\n");
-          dOut.writeUTF(" " + customerOrder.getId());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          if (label.getBillingLabel() != null) {
-            List<String> labelLines = Arrays.asList(label.getBillingLabel().split("\\R"));
-            if (labelLines != null) {
-              for (String line : labelLines) {
-                List<String> lineToPrint = new ArrayList<String>();
-                if (line.contains("\r\n")) {
-                  lineToPrint = Arrays.asList(line.split("\r\n"));
-                } else
-                  lineToPrint.add(line);
-                for (String lin : lineToPrint) {
-                  dOut.writeUTF("               " + StringUtils.stripAccents(lin).toUpperCase());
-                  dOut.writeUTF("\r\n");
-                  dOut.flush();
-=======
-            dOut.writeUTF("   "
-                    + StringUtils
-                            .stripAccents(
-                                    (label.getBillingLabelAddress() != null ? label.getBillingLabelAddress() : ""))
-                            .replaceAll("\\p{C}", "").toUpperCase()
-                    +
-                    "    "
-                    + (customerOrder.getTiers() != null ? StringUtils.stripAccents(
-                            (customerOrder.getTiers().getIntercom() != null ? customerOrder.getTiers().getIntercom()
-                                    : ""))
-                            .replaceAll("\\p{C}", "").toUpperCase() + " " : "")
-                    +
-                    (customerOrder.getResponsable() != null ? (customerOrder.getResponsable().getBuilding() != null
-                            ? (" | " + customerOrder.getResponsable().getBuilding())
-                            : "") + " " +
-                            (customerOrder.getResponsable().getFloor() != null
-                                    ? (" | " + customerOrder.getResponsable().getFloor())
-                                    : "")
-                            : ""));
-
-            dOut.flush();
-            dOut.writeUTF("\r\n");
-
-            dOut.writeUTF("   "
-                    + StringUtils.stripAccents(label.getBillingLabelCity() != null
-                            ? label.getBillingLabelCity().getLabel()
-                            : "").replaceAll("\\p{C}", "")
-                    + " " + (label.getBillingLabelComplementCedex() != null
-                            ? StringUtils.stripAccents(label.getBillingLabelComplementCedex()).toUpperCase()
-                                    .replaceAll("\\p{C}", "")
-                            : "")
-                    + " "
-                    + (label.getBillingLabelPostalCode() != null
-                            ? label.getBillingLabelPostalCode().replaceAll("\\p{C}", "")
-                            : "")
-                    + " "
-                    + ((label.getBillingLabelCountry() == null || label.getBillingLabelCountry().getId()
-                            .equals(constantService.getCountryFrance().getId())) ? ""
-                                    : label.getBillingLabelCountry().getLabel().replaceAll("\\p{C}", "")));
-
-            dOut.flush();
-            dOut.writeUTF("\r\n");
-            dOut.flush();
-
-        } catch (IOException e) {
-            throw new OsirisException(e, "Error when printing");
-        } finally {
-            if (dOut != null)
-                try {
-                    dOut.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
->>>>>>> develop
-                }
-              }
-            }
-          }
-
-          dOut.writeUTF(
-              "               " + StringUtils
-                  .stripAccents((label.getBillingLabelAddress() != null ? label.getBillingLabelAddress()
-                      : ""))
-                  .toUpperCase());
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF(
-              "               ");
-          if (customerOrder.getTiers() != null) {
-            dOut.writeUTF(StringUtils.stripAccents((customerOrder.getTiers().getIntercom() != null
-                ? customerOrder.getTiers().getIntercom()
-                : ""))
-                .toUpperCase() + " ");
-          }
-          if (customerOrder.getResponsable() != null) {
-            dOut.writeUTF((customerOrder.getResponsable().getBuilding() != null
-                ? (" | " + customerOrder.getResponsable().getBuilding())
-                : "") + " "
-                + (customerOrder.getResponsable().getFloor() != null
-                    ? (" | " + customerOrder.getResponsable().getFloor())
-                    : ""));
-          }
-          dOut.flush();
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-          dOut.writeUTF("               " + (label.getBillingLabelPostalCode() != null
-              ? label.getBillingLabelPostalCode()
-              : "") + " "
-              + (label.getBillingLabelCity() != null
-                  ? label.getBillingLabelCity().getLabel()
-                  : "")
-              + " "
-              + (label.getCedexComplement() != null
-                  ? StringUtils.stripAccents(label.getCedexComplement()).toUpperCase()
-                  : "")
-              + " "
-              + ((label.getBillingLabelCountry() == null || label
-                  .getBillingLabelCountry().getId()
-                  .equals(constantService.getCountryFrance().getId())) ? ""
-                      : label.getBillingLabelCountry().getLabel()));
-          dOut.writeUTF("\r\n");
-          dOut.flush();
-=======
-  @Value("${printer.label.ip}")
-  private String printerIp;
-
-  @Value("${printer.label.port}")
-  private Integer printerPort;
-
-  @Autowired
-  ConstantService constantService;
-
-  public void printMailingLabel(InvoiceLabelResult label, CustomerOrder customerOrder) throws OsirisException {
 
     Socket socket = null;
     DataOutputStream dOut = null;
     try {
       socket = new Socket(printerIp, printerPort);
       dOut = new DataOutputStream(socket.getOutputStream());
+
+      if (printProvisionRegister) {
+
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "       " + StringUtils
+                .stripAccents((provisionType != null ? provisionType
+                    : ""))
+                .toUpperCase());
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "            " + StringUtils
+                .stripAccents((affaireName != null ? affaireName
+                    : ""))
+                .toUpperCase());
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        if (affaireAddress != null) {
+          List<String> labelLines = Arrays.asList(affaireAddress.split("\\R"));
+          if (labelLines != null) {
+            for (String line : labelLines) {
+              List<String> lineToPrint = new ArrayList<String>();
+              if (line.contains("\r\n")) {
+                lineToPrint = Arrays.asList(line.split("\r\n"));
+              } else
+                lineToPrint.add(line);
+              for (String lin : lineToPrint) {
+                dOut.writeUTF("            " + StringUtils.stripAccents(lin).toUpperCase());
+                dOut.writeUTF("\r\n");
+                dOut.flush();
+              }
+            }
+          }
+        }
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF("             " + StringUtils.stripAccents((cpCedex != null
+            ? cpCedex
+            : ""))
+            .toUpperCase() + " ");
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "             " + StringUtils
+                .stripAccents((siren != null ? siren
+                    : ""))
+                .toUpperCase());
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "            " + StringUtils.stripAccents((legalFormLabel != null ? legalFormLabel
+                : ""))
+                .toUpperCase());
+        dOut.writeUTF("\r\n");
+        dOut.flush();
+        dOut.writeUTF(
+            "            " + StringUtils.stripAccents((registreAC != null ? registreAC
+                : ""))
+                .toUpperCase());
+      } else {
+
 
       // Handle manual adresses
       if (label.getBillingLabelCity() == null) {
@@ -358,7 +203,6 @@ public class PrintDelegate {
           label.setBillingLabelPostalCode("");
           for (int i = 1; i < lines.size(); i++)
             label.setBillingLabelPostalCode(label.getBillingLabelPostalCode() + " " + lines.get(i));
->>>>>>> develop
         }
       }
       dOut.writeUTF("\r\n");
@@ -385,28 +229,6 @@ public class PrintDelegate {
         }
       }
 
-<<<<<<< HEAD
-      } catch (IOException e) {
-        throw new OsirisException(e, "Error when printing");
-      } finally {
-        if (dOut != null)
-          try {
-            dOut.close();
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-      }
-
-      try {
-        if (socket != null)
-          socket.close();
-      } catch (IOException e) {
-        throw new OsirisException(e, "Error when printing");
-      }
-    }
-  }
-}
-=======
       dOut.writeUTF("   "
           + StringUtils
               .stripAccents(
@@ -471,4 +293,3 @@ public class PrintDelegate {
     }
   }
 }
->>>>>>> develop
