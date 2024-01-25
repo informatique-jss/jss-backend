18758c86455ecde176778f6127c12befe7aeaf1a
ESTEVES Miguel <mesteves@jss.fr>
2024-01-25 18:41:34 +0100

    25/01 status

--- client/src/app/modules/tiers/components/visit-prepa-tiers-responsible-info/visit-prepa-tiers-responsible-info.component.ts
+++ client/src/app/modules/tiers/components/visit-prepa-tiers-responsible-info/visit-prepa-tiers-responsible-info.component.ts
@@ -17,6 +17,9 @@ import { OrderingSearch } from 'src/app/modules/quotation/model/OrderingSearch';
 import { QuotationSearch } from 'src/app/modules/quotation/model/QuotationSearch';
 import { AffaireSearch } from 'src/app/modules/quotation/model/AffaireSearch';
 import { InvoiceSearch } from 'src/app/modules/invoicing/model/InvoiceSearch';
+import { TiersSearchResultService } from '../../services/tiers.search.result.service';
+import { TiersSearchResult } from '../../model/TiersSearchResult';
+import { LegalForm } from '../../../miscellaneous/model/LegalForm';
 
 @Component({
   selector: 'visit-prepa-tiers-responsible-info',
@@ -32,17 +35,21 @@ export class VisitPrepaTiersResponsibleInfoComponent implements OnInit, AfterCon
   rffSearch: RffSearch = {} as RffSearch;
 
   rff: Rff[] | undefined;
+  rffList: Rff[] = [];
 
   tiersRff: TiersRff[] | undefined;
   responsablesRff: ResponsablesRff[] | undefined;
+  responsableDummy: ResponsablesRff = {} as ResponsablesRff;
 
   tiersList: Tiers[] = [];
   rffSearchList: RffSearch[] = [];
-  goNext: boolean = false;
   responsablesList: Responsable[] = [];
 
   responsableSearch: TiersSearch = {} as TiersSearch;
   responsableSearchResult: ResponsableSearchResult[] = [];
+  tiersSearchResult: TiersSearchResult[] = [];
+
+  onlyResponsable: Responsable | undefined;
 
   orderingSearch: OrderingSearch = {} as OrderingSearch;
   quotationSearch: QuotationSearch = {} as QuotationSearch;
@@ -52,11 +59,14 @@ export class VisitPrepaTiersResponsibleInfoComponent implements OnInit, AfterCon
 
   displayedColumnsTiersRff: SortTableColumn<TiersRff>[] = [];
   displayedColumnsResponsablesRff: SortTableColumn<ResponsablesRff>[] = [];
+  availableColumnsTiersRff: SortTableColumn<TiersRff>[] = [];
+  availableColumnsResponsablesRff: SortTableColumn<ResponsablesRff>[] = [];
 
   constructor(private rffService: RffService,
     private changeDetectorRef: ChangeDetectorRef,
     protected tiersService: TiersService,
     protected responsableSearchResultService :ResponsableSearchResultService,
+    protected tiersSearchResultService :TiersSearchResultService,
   ) { }
 
   ngAfterContentChecked(): void {
@@ -68,24 +78,27 @@ export class VisitPrepaTiersResponsibleInfoComponent implements OnInit, AfterCon
 
     this.tiersList = [this.tiers];
     this.responsablesList = this.tiers.responsables;
-    this.searchRff();
+    this.rffSearchList = [{} as RffSearch];
+    this.rff = [{} as Rff];
+    this.rffSearch = {} as RffSearch;
+    this.responsableDummy = {} as ResponsablesRff;
 
     this.displayedColumnsTiersRff = [];
     this.displayedColumnsResponsablesRff = [];
 
     this.displayedColumnsTiersRff.push({ id: "denomination", fieldName: "denomination", label: "Dénomination"  } as SortTableColumn<TiersRff>);
     this.displayedColumnsTiersRff.push({ id: "address", fieldName: "address", label: "address" } as SortTableColumn<TiersRff>);
-    this.displayedColumnsTiersRff.push({ id: "mail", fieldName: "mail", label: "mail" } as SortTableColumn<TiersRff>);
-    this.displayedColumnsTiersRff.push({ id: "phone", fieldName: "phone", label: "phone" } as SortTableColumn<TiersRff>);
+    this.displayedColumnsTiersRff.push({ id: "mails", fieldName: "mails", label: "Mails", valueFonction: (element: TiersRff, column: SortTableColumn<TiersRff>) => { return ((element.mails) ? element.mails.map((e: { mail: any; }) => e.mail).join(", ") : "") } } as SortTableColumn<TiersRff>);
+    this.displayedColumnsTiersRff.push({ id: "phones", fieldName: "phones", label: "Téléphones", valueFonction: (element: TiersRff, column: SortTableColumn<TiersRff>) => { return ((element.phones) ? element.phones.map((e: { phoneNumber: any; }) => e.phoneNumber).join(", ") : "") } } as SortTableColumn<TiersRff>);
     this.displayedColumnsTiersRff.push({ id: "rffInsertion", fieldName: "rffInsertion", label: "RFF AL" } as SortTableColumn<TiersRff>);
     this.displayedColumnsTiersRff.push({ id: "rffFormalite", fieldName: "rffFormalite", label: "RFF Formalités" } as SortTableColumn<TiersRff>);
     this.displayedColumnsTiersRff.push({ id: "rffTotal", fieldName: "rffTotal", label: "Total HT", valueFonction: formatEurosForSortTable } as SortTableColumn<TiersRff>);
 
     this.displayedColumnsResponsablesRff.push({ id: "id", fieldName: "id", label: "N° du responsable" } as SortTableColumn<ResponsablesRff>);
     this.displayedColumnsResponsablesRff.push({ id: "lastName", fieldName: "lastName", label: "Nom" } as SortTableColumn<ResponsablesRff>);
-    this.displayedColumnsResponsablesRff.push({ id: "phone", fieldName: "phone", label: "Tel" } as SortTableColumn<ResponsablesRff>);
+    this.displayedColumnsResponsablesRff.push({ id: "phones", fieldName: "phones", label: "Téléphones", valueFonction: (element: ResponsablesRff, column: SortTableColumn<ResponsablesRff>) => { return ((element.phones) ? element.phones.map((e: { phoneNumber: any; }) => e.phoneNumber).join(", ") : "") } } as SortTableColumn<ResponsablesRff>);
     this.displayedColumnsResponsablesRff.push({ id: "function", fieldName: "function", label: "Function" } as SortTableColumn<ResponsablesRff>);
-    this.displayedColumnsResponsablesRff.push({ id: "mail", fieldName: "mail", label: "Mails"} as SortTableColumn<ResponsablesRff>);
+    this.displayedColumnsResponsablesRff.push({ id: "mails", fieldName: "mails", label: "Mails", valueFonction: (element: ResponsablesRff, column: SortTableColumn<ResponsablesRff>) => { return ((element.mails) ? element.mails.map((e: { mail: any; }) => e.mail).join(", ") : "") } } as SortTableColumn<ResponsablesRff>);
     this.displayedColumnsResponsablesRff.push({ id: "turnoverAmountWithTax", fieldName: "turnoverAmountWithTax", label: "Chiffre d'affaire", valueFonction: formatEurosForSortTable} as SortTableColumn<ResponsablesRff>);
     this.displayedColumnsResponsablesRff.push({ id: "announcementNbr", fieldName: "announcementNbr", label: "Nbre AL"} as SortTableColumn<ResponsablesRff>);
     this.displayedColumnsResponsablesRff.push({ id: "formalityNbr", fieldName: "formalityNbr", label: "Nbre Form"} as SortTableColumn<ResponsablesRff>);
@@ -93,106 +106,144 @@ export class VisitPrepaTiersResponsibleInfoComponent implements OnInit, AfterCon
     this.displayedColumnsResponsablesRff.push({ id: "rffFormalite", fieldName: "rffFormalite", label: "RFF Formalités" } as SortTableColumn<ResponsablesRff>);
     this.displayedColumnsResponsablesRff.push({ id: "rffTotal", fieldName: "rffTotal", label: "Total HT", valueFonction: formatEurosForSortTable } as SortTableColumn<ResponsablesRff>);
 
+    this.searchRff();
   }
 
   searchRff() {
 
+    if(this.tiers.responsables.length>1){
+      this.tiers.responsables.forEach(responsable => {
+        this.setRffSearch(responsable);
+      })
+    }else{
+        this.onlyResponsable = this.tiers.responsables[0];
+        this.setRffSearch(this.onlyResponsable);
+    }
 
-    this.tiers.responsables.forEach(responsable => {
-      this.rffSearch = {} as RffSearch;
-      this.rffSearch.tiers = { entityId: this.tiers.id } as IndexEntity;
-      this.rffSearch.isHideCancelledRff = false;
-      this.rffSearch.responsable = { entityId: responsable.id } as IndexEntity;
-      this.rffSearchList.push(this.rffSearch);
-
-      let start = new Date();
-      let d = new Date(start.getTime());
-      d.setFullYear(d.getFullYear() - 2);
-      d.setMonth(10);
-      d.setDate(30);
-      this.rffSearch.startDate = d;
-
-      let end = new Date();
-      let d2 = new Date(end.getTime());
-      d2.setFullYear(d2.getFullYear() - 1);
-      d2.setMonth(11);
-      d2.setDate(1);
-      this.rffSearch.endDate = d2;
-    })
-
-    this.rffSearchList.forEach(rffSearch => {
-      this.rffService.getRffs(rffSearch).subscribe(response => {
-        this.rff = response.sort((a, b) => a.responsableLabel.localeCompare(b.responsableLabel));
+    if(this.rffSearchList.length>1){
+      this.rffSearchList.forEach(rffSearch => {
+          this.rffService.getRffs(rffSearch).subscribe(response => {
+            this.rff = response;
+          });
       });
-      if(this.rff)
-      this.setTiersAndResponsableRffData(this.rff);
+    }
+
+      this.rffService.getRffs(this.rffSearchList[0]).subscribe(response => {
+        this.rff =response;
+      });
+
+      if(this.rff && this.rff?.length>1){
+        this.rff?.forEach( (rff: Rff) => {
+            if(rff.responsableId!=null)
+              this.rffList.push(rff)
+        });
+    }else if(this.rff && this.rff?.length == 1){
+      this.rffList.push(this.rff[0]);
+    }
+    this.setTiersAndResponsableRffData(this.rffList);
+  }
+
+  setRffSearch(responsable: Responsable) {
+
+        this.rffSearch.tiers = { entityId: this.tiers.id } as IndexEntity;
+        this.rffSearch.responsable = { entityId: responsable.id } as IndexEntity;
+        this.rffSearch.isHideCancelledRff = false;
+
+        let start = new Date();
+        let d = new Date(start.getTime());
+        d.setFullYear(d.getFullYear() - 2);
+        d.setMonth(10);
+        d.setDate(30);
+        this.rffSearch.startDate = d;
+
+        let end = new Date();
+        let d2 = new Date(end.getTime());
+        d2.setFullYear(d2.getFullYear() - 1);
+        d2.setMonth(11);
+        d2.setDate(1);
+        this.rffSearch.endDate = d2;
+
+        this.rffSearchList.push({ ...this.rffSearch });
+
+  }
+
+  setTiersAndResponsableRffData(rffList: Rff[]) {
+
+    this.tiersRff = [{} as TiersRff];
+    this.responsablesRff = [{} as ResponsablesRff];
+    this.responsableDummy = {} as ResponsablesRff;
+
+    this.tiersRff[0].rffInsertion = rffList[0]?.rffInsertion;
+    this.tiersRff[0].rffFormalite = rffList[0]?.rffFormalite;
+    this.tiersRff[0].rffTotal = rffList[0]?.rffTotal;
+
+    rffList.forEach(rffResponsable => {
+
+      if(rffResponsable.responsableId && this.responsableDummy && this.responsablesRff){
+        this.responsableDummy.id = rffResponsable.responsableId;
+        this.responsableDummy.rffInsertion = rffResponsable.rffInsertion;
+        this.responsableDummy.rffFormalite = rffResponsable.rffFormalite;
+        this.responsableDummy.rffTotal = rffResponsable.rffTotal;
+        this.responsablesRff.push(this.responsableDummy);
+      }
     })
+    this.setTiersAndResponsableData(this.tiersRff, this.responsablesRff);
   }
 
   setTiersAndResponsableData(tiersRff: TiersRff[] , responsablesRff: ResponsablesRff[] ){
+
+
+    tiersRff[0].id = this.tiers.id;
     if(tiersRff[0].denomination == null){
       if(!this.tiers.isIndividual)
         tiersRff[0].denomination = this.tiers.denomination;
       else
         tiersRff[0].denomination = this.tiers.firstname + " " + this.tiers.lastname;
-
       tiersRff[0].address = this.tiers.address + " - " + this.tiers.postalCode
       + " "+ this.tiers.city.label + " "+ this.tiers.country.label;
       tiersRff[0].mails = this.tiers.mails;
       tiersRff[0].phones = this.tiers.phones;
     }
+
     this.tiers.responsables.forEach(responsable => {
-      this.responsablesRff?.forEach(responsablesRff =>{
-        responsablesRff.id = responsable.id;
-        responsablesRff.lastName = responsable.lastname;
-        responsablesRff.firstName = responsable.firstname;
-        responsablesRff.function = responsable.function;
-        responsablesRff.mails = responsable.mails;
-        responsablesRff.phones = responsable.phones;
-     })
+      if(responsablesRff.length == 0){
+        this.responsableDummy.id = responsable.id;
+        this.responsableDummy.lastName = responsable.lastname;
+        this.responsableDummy.firstName = responsable.firstname;
+        this.responsableDummy.function = responsable.function;
+        this.responsableDummy.mails = responsable.mails;
+        this.responsableDummy.phones = responsable.phones;
+        this.responsablesRff?.push(this.responsableDummy);
+      }
     })
-     this.setResponsableSearchResultData(responsablesRff);
+     this.setResponsableSearchResultData(this.tiersRff, this.responsablesRff);
   }
 
-  setTiersAndResponsableRffData(rff: Rff[]) {
 
-    this.tiersRff = [{} as TiersRff];
-    this.responsablesRff = [{} as ResponsablesRff];
+  setResponsableSearchResultData(tiersRff: TiersRff[] | undefined , responsablesRff: ResponsablesRff[] | undefined) {
 
-    this.tiersRff[0].rffInsertion = rff[0]?.rffInsertion;
-    this.tiersRff[0].rffFormalite = rff[0]?.rffFormalite;
-    this.tiersRff[0].rffTotal = rff[0]?.rffTotal
-
-    rff.forEach(rff => {
-      this.responsablesRff?.forEach(responsablesRff =>{
-        responsablesRff.rffInsertion = rff.rffInsertion;
-        responsablesRff.rffFormalite = rff.rffFormalite;
-        responsablesRff.rffTotal = rff.rffTotal
-      })
-    })
-    this.setTiersAndResponsableData(this.tiersRff, this.responsablesRff);
-  }
-
-  setResponsableSearchResultData(responsablesRff: ResponsablesRff[]) {
     this.responsableSearch = {} as TiersSearch
+
+    this.responsableSearch.tiers = { entityId: this.tiers.id } as IndexEntity;
 
     this.tiers.responsables.forEach(responsable => {
       this.responsableSearch.responsable = { entityId: responsable.id } as IndexEntity;
 
-        responsablesRff.forEach(responsablesRff =>{
-        this.responsableSearchResultService.getResponsableSearch(this.responsableSearch).subscribe(response => {
-          this.responsableSearchResult = response;
-          if(response){
-            this.responsableSearchResult.forEach(responsableSearchResult => {
-              responsablesRff.turnoverAmountWithTax = responsableSearchResult.turnoverAmountWithTax;
-              responsablesRff.announcementNbr = responsableSearchResult.announcementNbr;
-              responsablesRff.formalityNbr = responsableSearchResult.formalityNbr;
+      this.responsableSearchResultService.getResponsableSearch(this.responsableSearch).subscribe(response => {
+        this.responsableSearchResult = response;
+        if(response){
+          this.responsableSearchResult.forEach(responsableSearchResult => {
+            responsablesRff?.forEach(responsablesRff => {
+              if(responsablesRff.id == responsableSearchResult.responsableId){
+                responsablesRff.turnoverAmountWithTax = responsableSearchResult.turnoverAmountWithTax;
+                responsablesRff.announcementNbr = responsableSearchResult.announcementNbr;
+                responsablesRff.formalityNbr = responsableSearchResult.formalityNbr;
+              }
             })
-          }
-        })
-        })
+          })
+        }
+      })
     })
-
   }
 
   loadQuotationFilter() {
--- client/src/app/modules/tiers/model/TiersRff.ts
+++ client/src/app/modules/tiers/model/TiersRff.ts
@@ -14,5 +14,5 @@ export interface TiersRff {
   lastName: string;
   firstName: string;
   responsables: Responsable[];
-
+  turnoverAmountWithoutTax: number;
 }
