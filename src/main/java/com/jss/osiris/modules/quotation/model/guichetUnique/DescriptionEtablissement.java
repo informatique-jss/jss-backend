09ed62206fcb71fc17ae707a55483e1f744b10f7
Pierre Coanet <pcoanet@jss.fr>
2024-07-11 16:01:33 +0200

    correction on deploy script rec and prod + jakarta libs on DescriptionEtablissement.java

--- src/main/java/com/jss/osiris/modules/quotation/model/guichetUnique/DescriptionEtablissement.java
+++ src/main/java/com/jss/osiris/modules/quotation/model/guichetUnique/DescriptionEtablissement.java
@@ -3,13 +3,15 @@ package com.jss.osiris.modules.quotation.model.guichetUnique;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 
 import com.jss.osiris.libs.search.model.DoNotAudit;
 import com.jss.osiris.modules.miscellaneous.model.IId;
