1e766a98fed274830b3d28fdf2b4e4606e583d5c
Pierre Coanet <pcoanet@jss.fr>
2024-07-15 18:05:46 +0200

    correction to display label service field in select referential servicefieldtype

---
+++ src/main/java/com/jss/osiris/modules/quotation/model/AssoServiceFieldType.java
@@ -0,0 +1,75 @@
+package com.jss.osiris.modules.quotation.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+import com.jss.osiris.libs.search.model.IndexedField;
+import com.jss.osiris.modules.miscellaneous.model.IId;
+
+@Entity
+@Table(indexes = { @Index(name = "idx_asso_service_field_type", columnList = "id_service") })
+public class AssoServiceFieldType implements Serializable, IId {
+
+    @Id
+    @SequenceGenerator(name = "asso_service_field_type_sequence", sequenceName = "asso_service_field_type_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "asso_service_field_type_sequence")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_service")
+    @IndexedField
+    private Service service;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_field_type")
+    @IndexedField
+    private ServiceFieldType serviceFieldType;
+
+    private Boolean isMandatory;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public ServiceFieldType getServiceFieldType() {
+        return serviceFieldType;
+    }
+
+    public void setServiceFieldType(ServiceFieldType serviceFieldType) {
+        this.serviceFieldType = serviceFieldType;
+    }
+
+    public Boolean getIsMandatory() {
+        return isMandatory;
+    }
+
+    public void setIsMandatory(Boolean isMandatory) {
+        this.isMandatory = isMandatory;
+    }
+
+}
