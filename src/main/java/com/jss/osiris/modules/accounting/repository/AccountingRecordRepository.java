fc549abd63f0d1d8ac8e10d7504f3976e698c34d
Alex Gapin <agapin@jss.fr>
2024-08-01 09:52:00 +0200

    fix

--- src/main/java/com/jss/osiris/modules/accounting/repository/AccountingRecordRepository.java
+++ src/main/java/com/jss/osiris/modules/accounting/repository/AccountingRecordRepository.java
@@ -1,6 +1,5 @@
 package com.jss.osiris.modules.accounting.repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,7 +12,6 @@ import org.springframework.data.repository.query.Param;
 import com.jss.osiris.libs.QueryCacheCrudRepository;
 import com.jss.osiris.modules.accounting.model.AccountingAccount;
 import com.jss.osiris.modules.accounting.model.AccountingBalance;
-import com.jss.osiris.modules.accounting.model.AccountingBalanceBilan;
 import com.jss.osiris.modules.accounting.model.AccountingJournal;
 import com.jss.osiris.modules.accounting.model.AccountingRecord;
 import com.jss.osiris.modules.accounting.model.AccountingRecordSearchResult;
@@ -132,7 +130,7 @@ public interface AccountingRecordRepository extends QueryCacheCrudRepository<Acc
                         "select  round(cast(sum(case "
                         + "            when record.isanouveau=false or record.is_from_as400=true then record.credit_amount "
                         + "        end)  as numeric), 2) as creditAmount," + "        round(cast(sum(case "
-                        + "            when record.isanouveau=false or record.is_from_as400=true then record.debit_amount "
+                        + "            when record.isanouveau=false or record.is_from_as400=true then round(cast(r.debit_amount as numeric), 2) "
                         + "        end) as numeric), 2) as debitAmount,"
                         + "		accounting.label as accountingAccountLabel,"
                         + "		pa.code as principalAccountingAccountCode,"
@@ -206,19 +204,6 @@ public interface AccountingRecordRepository extends QueryCacheCrudRepository<Acc
                         @Param("endDate") LocalDateTime endDate,
                         @Param("canViewRestricted") boolean canViewRestricted,
                         @Param("isFromAs400") Boolean isFromAs400);
-
-        @Query("select sum(a.creditAmount) as creditAmount, " +
-                        " sum(a.debitAmount) as debitAmount, pa.code as accountingAccountNumber "
-                        +
-                        " from AccountingRecord a  JOIN a.accountingAccount aa  JOIN aa.principalAccountingAccount pa  where "
-                        +
-                        "( (coalesce(a.manualAccountingDocumentDate,a.operationDateTime) >=:startDate and coalesce(a.manualAccountingDocumentDate,a.operationDateTime) <=:endDate ))   "
-                        + " and (:canViewRestricted=true or aa.isViewRestricted=false)  " +
-                        " group by pa.code ")
-        List<AccountingBalanceBilan> getAccountingRecordAggregateByAccountingNumber(
-                        @Param("startDate") LocalDate startDate,
-                        @Param("endDate") LocalDate endDate,
-                        @Param("canViewRestricted") boolean canViewRestricted);
 
         List<AccountingRecord> findByAccountingAccountAndInvoice(AccountingAccount accountingAccountCustomer,
                         Invoice invoice);
