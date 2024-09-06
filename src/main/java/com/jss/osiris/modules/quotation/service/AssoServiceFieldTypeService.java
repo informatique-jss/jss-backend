1602dd3e4952a809b706a804fd6bb0cffd659af1
Pierre Coanet <pcoanet@jss.fr>
2024-09-06 15:20:35 +0200

    correction on rollback function for service assServiceFieldTypeService

--- src/main/java/com/jss/osiris/modules/quotation/service/AssoServiceFieldTypeService.java
+++ src/main/java/com/jss/osiris/modules/quotation/service/AssoServiceFieldTypeService.java
@@ -1,11 +1,9 @@
 package com.jss.osiris.modules.quotation.service;
 
-import com.jss.osiris.libs.exception.OsirisException;
 import com.jss.osiris.modules.quotation.model.AssoServiceFieldType;
 
 public interface AssoServiceFieldTypeService {
     public AssoServiceFieldType getAssoServiceFieldType(Integer id);
 
-    public AssoServiceFieldType addOrUpdateServiceFieldType(AssoServiceFieldType assoServiceFieldType)
-            throws OsirisException;
+    public AssoServiceFieldType addOrUpdateServiceFieldType(AssoServiceFieldType assoServiceFieldType);
 }
--- src/main/java/com/jss/osiris/modules/quotation/service/AssoServiceFieldTypeServiceImpl.java
+++ src/main/java/com/jss/osiris/modules/quotation/service/AssoServiceFieldTypeServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.jss.osiris.libs.exception.OsirisException;
 import com.jss.osiris.modules.quotation.model.AssoServiceFieldType;
 import com.jss.osiris.modules.quotation.repository.AssoServiceFieldTypeRepository;
 
@@ -14,9 +13,6 @@ public class AssoServiceFieldTypeServiceImpl implements AssoServiceFieldTypeServ
 
     @Autowired
     AssoServiceFieldTypeRepository assoServiceFieldTypeRepository;
-
-    @Autowired
-    MissingAttachmentQueryService missingAttachmentQueryService;
 
     @Override
     public AssoServiceFieldType getAssoServiceFieldType(Integer id) {
@@ -28,9 +24,7 @@ public class AssoServiceFieldTypeServiceImpl implements AssoServiceFieldTypeServ
 
     @Override
     @org.springframework.transaction.annotation.Transactional(rollbackFor = Exception.class)
-    public AssoServiceFieldType addOrUpdateServiceFieldType(AssoServiceFieldType assoServiceFieldType)
-            throws OsirisException {
-        missingAttachmentQueryService.checkCompleteAttachmentAndFieldListAndComment(null, assoServiceFieldType, null);
+    public AssoServiceFieldType addOrUpdateServiceFieldType(AssoServiceFieldType assoServiceFieldType) {
         return assoServiceFieldTypeRepository.save(assoServiceFieldType);
     }
 }
