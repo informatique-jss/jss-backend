a2b42dccf42083a356a793334d9e7f89e33dff52
Pierre Coanet <pcoanet@jss.fr>
2024-09-09 14:32:58 +0200

    correction on filter repo

--- src/main/java/com/jss/osiris/modules/quotation/repository/CustomerOrderRepository.java
+++ src/main/java/com/jss/osiris/modules/quotation/repository/CustomerOrderRepository.java
@@ -146,10 +146,9 @@ public interface CustomerOrderRepository extends QueryCacheCrudRepository<Custom
                         + " and co.created_date>=:startDate and co.created_date<=:endDate "
                         + " and ( COALESCE(:assignedToEmployee) =0 or co.id_assigned_to in (:assignedToEmployee))"
                         + " and ( COALESCE(:salesEmployee) =0 or cf.id_commercial in (:salesEmployee) or r.id_commercial in (:salesEmployee) or t.id_commercial in (:salesEmployee) or t.id_commercial is null and t2.id_commercial in (:salesEmployee))"
-                        + " and ( COALESCE(:activeDirectoryGroupId)=0 or exists (select :activeDirectoryGroupId from customer_order_comment com2 "
-                        + " join asso_customer_order_comment_active_directory_group asso_grp2 on com2.id = asso_grp2.id_customer_order_comment "
+                        + " and ( COALESCE(:activeDirectoryGroupId)=0 or exists (select 1 from asso_customer_order_comment_active_directory_group asso_grp2 "
                         + " join active_directory_group grp2 on asso_grp2.id_active_directory_group = grp2.id "
-                        + " where grp2.id =:activeDirectoryGroupId and  co.id= com2.id_customer_order and com.id=com2.id) ) "
+                        + " where grp2.id =:activeDirectoryGroupId and asso_grp2.id_customer_order_comment = com.id) ) "
                         + " and ( :isDisplayOnlyUnread and COALESCE(com.is_read,false)=false or :isDisplayOnlyUnread=false) "
                         + " group by cf.id, cf.label, r.id, r.firstname,origin.label,  r.lastname, t.denomination, t.firstname, t.lastname, t2.denomination, t2.firstname, t2.lastname, cos.label, "
                         + " co.created_date, cf.id_commercial, r.id_commercial, t.id_commercial, t2.id_commercial, co.id, r.id, t.id,t2.id, cf.id, co.description,co.id_assigned_to ")
