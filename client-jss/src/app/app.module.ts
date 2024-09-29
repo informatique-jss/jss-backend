e77c43c60ee713b38b3beb57fa5b2fdd9ec55d5c
Alex Gapin <agapin@jss.fr>
2024-09-29 17:47:59 +0200

    fix

--- client-jss/src/app/app.module.ts
+++ client-jss/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { registerLocaleData } from '@angular/common';
-import { provideHttpClient, withFetch, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import localeFr from '@angular/common/locales/fr';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -11,17 +11,19 @@ import { routes } from './app.routes';
 import { MainModule } from './main/components/main/main.module';
 registerLocaleData(localeFr, 'fr');
 
-@NgModule({ declarations: [
-        AppComponent,
-    ],
-    exports: [],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        RouterModule,
-        MainModule], providers: [provideRouter(routes),
-        provideClientHydration(),
-        provideHttpClient(withFetch()),
-        { provide: LOCALE_ID, useValue: 'fr' }, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  exports: [],
+  bootstrap: [AppComponent], imports: [BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule,
+    MainModule], providers: [provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    { provide: LOCALE_ID, useValue: 'fr' }, provideHttpClient(withInterceptorsFromDi())]
+})
 export class AppModule { }
 
