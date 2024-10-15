5c6322a834baa9489c7b0c1653b0eeeb00e5ef44
Pierre Coanet <pcoanet@jss.fr>
2024-10-15 09:56:40 +0200

    remove '/browser' from path in folder /dist/osiris/ because of rollback from Angular 18 to version 16

--- tools/deploy-prod.sh
+++ tools/deploy-prod.sh
@@ -12,5 +12,5 @@ cd client
 npm install  --legacy-peer-deps
 ng build --configuration production
 ssh -t osiris@osiris.jss.fr 'rm -R /appli/osiris/*;exit'
-scp -r dist/osiris/browser/* osiris@osiris.jss.fr:/appli/osiris/.
+scp -r dist/osiris/* osiris@osiris.jss.fr:/appli/osiris/.
 ssh -t osiris@osiris.jss.fr 'chown osiris:appli /appli/osiris/*;exit'
