b855da9bfb19528bb0a3a2a8d5fd4af638c99535
Pierre Coanet <pcoanet@jss.fr>
2024-10-17 15:41:46 +0200

    Merge fix cherrypick

--- src/main/java/com/jss/osiris/modules/quotation/repository/BankTransfertRepository.java
+++ src/main/java/com/jss/osiris/modules/quotation/repository/BankTransfertRepository.java
@@ -35,7 +35,6 @@ public interface BankTransfertRepository extends QueryCacheCrudRepository<BankTr
                         + " and r.transfert_date_time>=:startDate and r.transfert_date_time<=:endDate "
                         + " and (:minAmount is null or r.transfert_amount>=CAST(CAST(:minAmount as text) as real) ) "
                         + " and (:maxAmount is null or r.transfert_amount<=CAST(CAST(:maxAmount as text) as real) )"
-                        + " and (:idProvider=0 or provider.id=:idProvider) "
                         + " and (:label is null or  CAST(r.id as text) = upper(CAST(:label as text)) or  upper(r.label)  like '%' || upper(CAST(:label as text))  || '%' )")
         List<BankTransfertSearchResult> findTransferts(
                         @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate,
