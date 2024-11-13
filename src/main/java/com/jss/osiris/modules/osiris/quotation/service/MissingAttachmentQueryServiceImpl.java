ee9fb162d9347db61859cd7ffd4c89965cb8c069
Pierre Coanet <pcoanet@jss.fr>
2024-11-13 15:05:20 +0100

    correct PM and add status

--- src/main/java/com/jss/osiris/modules/osiris/quotation/service/MissingAttachmentQueryServiceImpl.java
+++ src/main/java/com/jss/osiris/modules/osiris/quotation/service/MissingAttachmentQueryServiceImpl.java
@@ -20,6 +20,7 @@ import com.jss.osiris.modules.osiris.quotation.model.AnnouncementStatus;
 import com.jss.osiris.modules.osiris.quotation.model.AssoServiceDocument;
 import com.jss.osiris.modules.osiris.quotation.model.AssoServiceFieldType;
 import com.jss.osiris.modules.osiris.quotation.model.CustomerOrderComment;
+import com.jss.osiris.modules.osiris.quotation.model.CustomerOrderStatus;
 import com.jss.osiris.modules.osiris.quotation.model.FormaliteStatus;
 import com.jss.osiris.modules.osiris.quotation.model.MissingAttachmentQuery;
 import com.jss.osiris.modules.osiris.quotation.model.Provision;
@@ -62,6 +63,9 @@ public class MissingAttachmentQueryServiceImpl implements MissingAttachmentQuery
 
     @Autowired
     CustomerOrderCommentService customerOrderCommentService;
+
+    @Autowired
+    CustomerOrderStatusService customerOrderStatusService;
 
     @Override
     public MissingAttachmentQuery getMissingAttachmentQuery(Integer id) {
@@ -125,19 +129,24 @@ public class MissingAttachmentQueryServiceImpl implements MissingAttachmentQuery
             query.setCreatedDateTime(LocalDateTime.now());
             toSend = true;
         } else {
-            if (query.getFirstCustomerReminderDateTime() == null
-                    && (isForcedReminder || query.getCreatedDateTime().isBefore(LocalDateTime.now().minusDays(10)))) {
-                query.setFirstCustomerReminderDateTime(LocalDateTime.now());
-                toSend = true;
-            } else if (query.getSecondCustomerReminderDateTime() == null && (isForcedReminder
-                    || query.getFirstCustomerReminderDateTime().isBefore(LocalDateTime.now().minusDays(10)))) {
-                query.setSecondCustomerReminderDateTime(LocalDateTime.now());
-                toSend = true;
-            } else if (query.getThirdCustomerReminderDateTime() == null && (isForcedReminder
-                    || query.getSecondCustomerReminderDateTime().isBefore(LocalDateTime.now().minusDays(10)))) {
-                isLastReminder = true;
-                query.setThirdCustomerReminderDateTime(LocalDateTime.now());
-                toSend = true;
+            if (query.getFirstCustomerReminderDateTime() == null) {
+                if (isForcedReminder || query.getCreatedDateTime().isBefore(LocalDateTime.now().minusDays(10))) {
+                    query.setFirstCustomerReminderDateTime(LocalDateTime.now());
+                    toSend = true;
+                }
+            } else if (query.getSecondCustomerReminderDateTime() == null) {
+                if (isForcedReminder
+                        || query.getFirstCustomerReminderDateTime().isBefore(LocalDateTime.now().minusDays(10))) {
+                    query.setSecondCustomerReminderDateTime(LocalDateTime.now());
+                    toSend = true;
+                }
+            } else if (query.getThirdCustomerReminderDateTime() == null) {
+                if (isForcedReminder
+                        || query.getSecondCustomerReminderDateTime().isBefore(LocalDateTime.now().minusDays(10))) {
+                    isLastReminder = true;
+                    query.setThirdCustomerReminderDateTime(LocalDateTime.now());
+                    toSend = true;
+                }
             }
         }
         query.setService(service);
@@ -157,7 +166,8 @@ public class MissingAttachmentQueryServiceImpl implements MissingAttachmentQuery
                         .getSimpleProvisionStatusByCode(SimpleProvisionStatus.SIMPLE_PROVISION_WAITING_DOCUMENT)
                         .getId(),
                         formaliteStatusService.getFormaliteStatusByCode(FormaliteStatus.FORMALITE_WAITING_DOCUMENT)
-                                .getId());
+                                .getId(),
+                        customerOrderStatusService.getCustomerOrderStatusByCode(CustomerOrderStatus.ABANDONED).getId());
 
         if (missingAttachmentQueries != null && missingAttachmentQueries.size() > 0) {
             for (MissingAttachmentQuery missingAttachmentQuery : missingAttachmentQueries) {
