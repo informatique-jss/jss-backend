0b7cf468c55989bb66b389c18f321f4c245c2b0b
cocoJss <pcoanet@jss.fr>
2025-02-20 16:23:46 +0100

    change condition where on mail filter for responsable and tiers repository

--- src/main/java/com/jss/osiris/modules/osiris/tiers/repository/ResponsableRepository.java
+++ src/main/java/com/jss/osiris/modules/osiris/tiers/repository/ResponsableRepository.java
@@ -111,7 +111,7 @@ public interface ResponsableRepository extends QueryCacheCrudRepository<Responsa
                         "  ( :tiersId =0 or t.id = :tiersId) " +
                         " and  ( :salesEmployeeId =0 or e2.id = :salesEmployeeId) " +
                         " and  ( :responsableId =0 or r.id = :responsableId) " +
-                        " and ( :mail='' or exists (select 1 from asso_tiers_mail a join mail m on m.id = a.id_mail where t.id = a.id_tiers and m.mail like '%' || trim(:mail)  || '%')) "
+                        " and ( :mail='' or exists (select 1 from mail m  where r.id_mail = m.id and m.mail like '%' || lower(trim(:mail))  || '%')) "
                         +
                         " and (CAST(:label as text) ='' or CAST(r.id as text) = upper(CAST(:label as text)) or  upper(concat(r.firstname, ' ',r.lastname))  like '%' || trim(upper(CAST(:label as text)))  || '%' or  upper(t.denomination)  like '%' || trim(upper(CAST(:label as text)))  || '%'  ) "
                         +
