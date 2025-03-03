139b4ec462fe99f5dc7d954f6d545d92e52ce82d
cocoJss <pcoanet@jss.fr>
2025-03-03 17:52:12 +0100

    save devs carousel generic component

--- client-myjss/src/app/modules/miscellaneous/components/miscellaneous/miscellaneous.module.ts
+++ client-myjss/src/app/modules/miscellaneous/components/miscellaneous/miscellaneous.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes } from '@angular/router';
+import { DragScrollComponent, DragScrollItemDirective } from 'ngx-drag-scroll';
 import { TrustHtmlPipe } from '../../../../libs/TrustHtmlPipe';
 import { AvatarComponent } from '../avatar/avatar.component';
+import { CarouselComponent } from '../carousel/carousel.component';
 import { GenericInputComponent } from '../forms/generic-input/generic-input.component';
 import { GenericTextareaComponent } from '../forms/generic-textarea/generic-textarea.component';
 import { SingleUploadComponent } from '../forms/single-upload/single-upload.component';
@@ -22,13 +24,17 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    DragScrollComponent,
+    DragScrollItemDirective,
+
   ],
   declarations: [MiscellaneousComponent,
     AvatarComponent,
     GenericInputComponent,
     SingleUploadComponent,
     TrustHtmlPipe,
-    GenericTextareaComponent
+    GenericTextareaComponent,
+    CarouselComponent
   ],
   exports: [
     AvatarComponent,
@@ -36,6 +42,7 @@ const routes: Routes = [
     SingleUploadComponent,
     TrustHtmlPipe,
     GenericTextareaComponent,
+    CarouselComponent
   ]
 })
 export class MiscellaneousModule { }
