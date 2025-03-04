707153b64d7b9849d557f3296e4f94c130cc36ef
BOXEBELD Frédéric <fboxebeld@jss.fr>
2025-03-04 18:22:04 +0100

    First version of clients section + correction of last PR review on homepage

--- client-myjss/src/app/modules/general/components/our-clients/our-clients.component.ts
+++ client-myjss/src/app/modules/general/components/our-clients/our-clients.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-our-clients',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-clients.component.css']
 })
 export class OurClientsComponent implements OnInit {
+
+  engieLogo: string = "assets/images/engie-logo.png";
+
+  @Input()
+  scrollDirection: 'normal' | 'reverse' = 'normal';
 
   constructor() { }
 
