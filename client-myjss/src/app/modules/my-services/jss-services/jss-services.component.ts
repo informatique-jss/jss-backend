80ea4aaf35d08d10fc20f24211c56b98d1ff2750
cocoJss <pcoanet@jss.fr>
2025-03-07 11:44:38 +0100

    corrections after code review the 06-03-25

--- client-myjss/src/app/modules/my-services/jss-services/jss-services.component.ts
+++ client-myjss/src/app/modules/my-services/jss-services/jss-services.component.ts
@@ -19,21 +19,16 @@ export class JssServicesComponent implements OnInit {
     { id: 'document', name: 'Fourniture de documents', active: false }
   ];
 
-  // Initialisation du breadcrumb avec "Services"
   breadcrumbItems = ['Services'];
 
-  // Gestion du clic sur un onglet
   onTabClick(selectedIndex: number): void {
-    // Réinitialisation de l'état des onglets
     this.tabs.forEach((tab, index) => {
       tab.active = index === selectedIndex;
     });
 
-    // Mettre à jour dynamiquement le breadcrumb avec le nom de l'onglet sélectionné
     this.updateBreadcrumb(this.tabs[selectedIndex]);
   }
 
-  // Mettre à jour le breadcrumb
   updateBreadcrumb(tab: any): void {
     this.breadcrumbItems = ['Services', tab.name];
   }
