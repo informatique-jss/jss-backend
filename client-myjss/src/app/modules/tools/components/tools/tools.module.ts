58f99d7ac15a47f5289301da382bddc5f2f6cd76
cocoJss <pcoanet@jss.fr>
2025-03-24 12:39:30 +0100

    change tools module tree project

--- client-myjss/src/app/modules/tools/components/tools/tools.module.ts
+++ client-myjss/src/app/modules/tools/components/tools/tools.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MiscellaneousModule } from '../../../miscellaneous/components/miscellaneous/miscellaneous.module';
 import { PostComponent } from '../post/post.component';
+import { PracticalSheetsComponent } from '../practical-sheets/practical-sheets.component';
 import { ToolsComponent } from './tools.component';
 
 const routes: Routes = [
-  { path: 'tools', component: ToolsComponent },
+  {
+    path: 'tools', component: ToolsComponent,
+    children: [
+      { path: 'practical-sheets', component: PracticalSheetsComponent },
+    ]
+  },
   { path: 'post/:slug', component: PostComponent },
 ];
 
@@ -21,6 +27,6 @@ const routes: Routes = [
   declarations: [
     ToolsComponent,
     PostComponent
-  ]
+  ], schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class ToolsModule { }
