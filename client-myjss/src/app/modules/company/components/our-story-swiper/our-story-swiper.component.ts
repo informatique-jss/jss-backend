058400ebde628e4ebcf37bbf88cd9c12e575d6a2
BOXEBELD Frédéric <fboxebeld@jss.fr>
2025-04-02 15:23:21 +0200

    Revert "Correcting after QA"
    This reverts commit bf7a3269b5ecc8b602cac98552da706a24121de3.

--- client-myjss/src/app/modules/company/components/our-story-swiper/our-story-swiper.component.ts
+++ client-myjss/src/app/modules/company/components/our-story-swiper/our-story-swiper.component.ts
@@ -3,14 +3,30 @@ import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, V
 import { SwiperContainer } from 'swiper/element';
 
 @Component({
-  selector: 'our-story-swiper',
-  templateUrl: './our-story-swiper.component.html',
-  styleUrls: ['./our-story-swiper.component.css'],
-  imports: [CommonModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  standalone: true
+    selector: 'our-story-swiper',
+    templateUrl: './our-story-swiper.component.html',
+    styleUrls: ['./our-story-swiper.component.css'],
+    imports: [CommonModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class OurStorySwiperComponent implements OnInit, AfterViewInit {
+
+
+  history = [
+    { year: 1898, category: "JOURNAL SPÉCIAL DES SOCIÉTÉS FRANÇAISES PAR ACTIONS", text: 'Création du supplément de l’annuaire général des sociétés françaises par actions', image: "assets/images/image-story-1898.png" },
+    { year: 1900, category: "ANNONCES LÉGALES", text: 'Première habitation dans le département de la Seine', image: "assets/images/image-story-1898.png" },
+    { year: 1985, category: "FORMALITÉS", text: 'Création du service de formalité légales', image: "assets/images/image-story-1898.png" },
+    { year: 2002, category: "SITE WEB", text: 'Lancement du site internet jss.fr', image: "assets/images/image-story-1898.png" },
+    { year: 2003, category: "FORMATIONS", text: 'Création du service des formations juridiques', image: "assets/images/image-story-1898.png" },
+    { year: 2004, category: "", text: 'Création de la revue juridique, “le Journal des Sociétés”, mensuel du juriste et de l’entreprise', image: "assets/images/image-story-2004.png" },
+    { year: 2014, category: "TRAITEMENT DES FORMALITÉS", text: 'Création du service de formalités dématérialisée', image: "assets/images/image-story-1898.png" },
+    { year: 2015, category: "CROISSANCE EXTERNE", text: 'Rachat du Journal “les Annonces de la Seine"', image: "assets/images/image-story-2015.png" },
+    { year: 2016, category: "ANNONCES LÉGALES", text: 'Nouvelles habilitations dans les départements 78, 91 et 95', image: "assets/images/image-story-1898.png" },
+    { year: 2019, category: "DOMICILIATION", text: 'Création du Service Domiciliation', image: "assets/images/image-story-1898.png" },
+    { year: 2020, category: "ANNONCES LÉGALES", text: 'Ouverture du Service de Presse de En Ligne (SPEL)', image: "assets/images/image-story-2020.png" },
+    { year: 2022, category: "JOURNAL SPÉCIAL DES SOCIÉTÉS", text: 'Le journal devient hebdomadaire', image: "assets/images/image-story-1898.png" },
+    { year: 2023, category: "JOURNAL SPÉCIAL DES SOCIÉTÉS", text: 'Digitalisation du journal', image: "assets/images/image-story-1898.png" }
+  ];
 
   @ViewChild('mainSwiper') mainSwiper!: ElementRef<SwiperContainer>;
   @ViewChild('datesSwiper') datesSwiper!: ElementRef<SwiperContainer>;
