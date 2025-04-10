3ce6e0a3df588741cbfcc4916a7a7eb443091684
cocoJss <pcoanet@jss.fr>
2025-04-10 11:30:32 +0200

    remove console.log (:p)

--- client-myjss/src/app/modules/tools/components/practical-sheets/practical-sheets.component.ts
+++ client-myjss/src/app/modules/tools/components/practical-sheets/practical-sheets.component.ts
@@ -152,8 +152,6 @@ export class PracticalSheetsComponent implements OnInit {
   }
 
   toggleCard(myJssCategory: MyJssCategory): void {
-    console.log(this.expandedCardIndex);
-    console.log(myJssCategory.id);
     if (this.expandedCardIndex === myJssCategory.id)
       this.expandedCardIndex = -1;
     else if (myJssCategory.id)
