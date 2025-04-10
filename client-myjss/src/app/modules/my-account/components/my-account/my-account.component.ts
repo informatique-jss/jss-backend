aa17aa456215bfd83e5d09cf11b9fb105c640b15
BOXEBELD Frédéric <fboxebeld@jss.fr>
2025-04-10 11:27:05 +0200

    added around theme.css for account routes except singin

--- client-myjss/src/app/modules/my-account/components/my-account/my-account.component.ts
+++ client-myjss/src/app/modules/my-account/components/my-account/my-account.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ThemeService } from '../../../../libs/theme.service';
 
 @Component({
-    selector: '.my-account',
-    templateUrl: './my-account.component.html',
-    styleUrls: ['./my-account.component.css'],
-    standalone: false
+  selector: '.my-account',
+  templateUrl: './my-account.component.html',
+  styleUrls: ['./my-account.component.css'],
+  standalone: false
 })
 export class MyAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private themeService: ThemeService
+  ) { }
 
   ngOnInit() {
   }
-
 }
