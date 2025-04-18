816e28177b316c8c6496f4f26a2af5bed50408e4
Alex Gapin <agapin@jss.fr>
2025-04-18 19:41:16 +0200

    remove legacy code

--- client-jss/src/app/main/components/main/main.module.ts
+++ client-jss/src/app/main/components/main/main.module.ts
@@ -5,21 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TimeFormatPipe } from '../../../libs/TimeFormatPipe';
 import { TrustHtmlPipe } from '../../../libs/TrustHtmlPipe';
 import { AnnouncementComponent } from '../announcement/announcement.component';
-import { ArticlesCategoryComponent } from '../articles-category/articles-category.component';
 import { AuthorHubComponent } from '../author-hub/author-hub.component';
-import { AuthorListComponent } from '../author-list/author-list.component';
-import { BodyArticlesComponent } from '../body-articles/body-articles.component';
 import { CategoryHubComponent } from '../category-hub/category-hub.component';
 import { DepartmentHubComponent } from '../department-hub/department-hub.component';
-import { DepartmentListComponent } from '../department-list/department-list.component';
 import { GenericInputComponent } from '../generic-input/generic-input.component';
 import { HeaderComponent } from '../header/header.component';
 import { IdfHubComponent } from '../idf-hub/idf-hub.component';
-import { InterviewListComponent } from '../interview-list/interview-list.component';
-import { NewArticlesComponent } from '../new-articles/new-articles.component';
 import { NewsletterComponent } from '../newsletter/newsletter.component';
-import { PodcastListComponent } from '../podcast-list/podcast-list.component';
-import { PodcastPostComponent } from '../podcast-post/podcast-post.component';
 import { PostAuthorHeaderComponent } from '../post-author-header/post-author-header.component';
 import { PostCategoryHeaderComponent } from '../post-category-header/post-category-header.component';
 import { PostDepartmentHeaderComponent } from '../post-department-header/post-department-header.component';
@@ -29,11 +21,7 @@ import { PostTagHeaderComponent } from '../post-tag-header/post-tag-hub-header.c
 import { PostComponent } from '../post/post.component';
 import { SearchAnnouncementComponent } from '../search-announcement/search-announcement.component';
 import { SerieHubComponent } from '../serie-hub/serie-hub.component';
-import { SerieListComponent } from '../serie-list/serie-list.component';
-import { SerieComponent } from '../serie/serie.component';
 import { TagHubComponent } from '../tag-hub/tag-hub.component';
-import { TagListComponent } from '../tag-list/tag-list.component';
-import { TrendComponent } from '../trend/trend.component';
 import { MainComponent } from './main.component';
 
 const routes: Routes = [
@@ -45,15 +33,6 @@ const routes: Routes = [
   { path: 'post/department/idf/all', component: PostIdfHeaderComponent },
   { path: 'home', component: MainComponent },
   { path: 'post/:slug', component: PostComponent },
-  { path: 'author/:slug', component: AuthorListComponent },
-  { path: 'category/:slug', component: ArticlesCategoryComponent },
-  { path: 'podcasts', component: PodcastListComponent },
-  { path: 'podcast/:slug', component: PodcastPostComponent },
-  { path: 'tag/:slug', component: TagListComponent },
-  { path: 'interviews', component: InterviewListComponent },
-  { path: 'series', component: SerieListComponent },
-  { path: 'serie/:slug', component: SerieComponent },
-  { path: 'department/:code', component: DepartmentListComponent },
   { path: 'announcement/search', component: SearchAnnouncementComponent },
   { path: 'announcement/:id', component: AnnouncementComponent },
 ];
@@ -69,21 +48,9 @@ const routes: Routes = [
   declarations: [
     MainComponent,
     HeaderComponent,
-    TrendComponent,
-    NewArticlesComponent,
     TrustHtmlPipe,
-    BodyArticlesComponent,
-    ArticlesCategoryComponent,
     PostComponent,
-    DepartmentListComponent,
-    InterviewListComponent,
-    SerieListComponent,
-    PodcastListComponent,
-    AuthorListComponent,
-    PodcastPostComponent,
-    SerieComponent,
     SearchAnnouncementComponent,
-    TagListComponent,
     TimeFormatPipe,
     AnnouncementComponent,
     CategoryHubComponent,
