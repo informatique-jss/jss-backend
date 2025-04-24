78d3e6b7ac2bf76dc0eac358dc52495d27107b0c
Alex Gapin <agapin@jss.fr>
2025-04-24 20:23:29 +0200

    fix

--- src/main/java/com/jss/osiris/modules/osiris/quotation/repository/guichetUnique/AssoServiceTypeDocumentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jss.osiris.modules.osiris.quotation.repository.guichetUnique;
-
-import com.jss.osiris.libs.QueryCacheCrudRepository;
-import com.jss.osiris.modules.osiris.quotation.model.AssoServiceTypeDocument;
-
-public interface AssoServiceTypeDocumentRepository extends QueryCacheCrudRepository<AssoServiceTypeDocument, String> {
-
-}
