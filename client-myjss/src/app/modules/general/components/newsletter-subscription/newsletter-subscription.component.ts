526a0cc3c3ad8febc788c3fa16a27597878e85a9
cocoJss <pcoanet@jss.fr>
2025-05-13 16:23:45 +0200

    newsletter webpage myjss

--- client-myjss/src/app/modules/general/components/newsletter-subscription/newsletter-subscription.component.ts
+++ client-myjss/src/app/modules/general/components/newsletter-subscription/newsletter-subscription.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AppService } from '../../../../libs/app.service';
+import { validateEmail } from '../../../../libs/CustomFormsValidatorsHelper';
+import { CommunicationPreferencesService } from '../../../my-account/services/communication.preference.service';
 
 @Component({
   selector: 'newsletter-subscription',
@@ -8,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsletterSubscriptionComponent implements OnInit {
 
-  constructor() { }
+  mail: string = '';
+
+  constructor(
+    private communicationPreferencesService: CommunicationPreferencesService,
+    private appService: AppService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
   }
+  newsletterForm = this.formBuilder.group({});
+  registerEmail(mailToRegister: string) {
+    if (!mailToRegister) {
+      return;
+    }
 
+    if (!validateEmail(mailToRegister)) {
+      this.appService.displayToast("Impossible de finaliser votre inscription. Vérifiez votre adresse e-mail et réessayez.", true, "Une erreur s’est produite...", 3000);
+      return;
+    }
+
+    this.communicationPreferencesService.subscribeToCorporateNewsletter(mailToRegister).subscribe(response => {
+      if (response) {
+        this.newsletterForm.reset();
+        this.mail = "";
+      }
+    });
+  }
 }
