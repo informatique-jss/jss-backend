8b24e887a968e6fa185aebf26826e3440599a131
Alex Gapin <agapin@jss.fr>
2025-05-27 16:10:21 +0200

    fix

--- src/main/java/com/jss/osiris/modules/osiris/quotation/service/guichetUnique/FormaliteGuichetUniqueServiceImpl.java
+++ src/main/java/com/jss/osiris/modules/osiris/quotation/service/guichetUnique/FormaliteGuichetUniqueServiceImpl.java
@@ -439,14 +439,18 @@ public class FormaliteGuichetUniqueServiceImpl implements FormaliteGuichetUnique
 
             savedFormaliteGuichetUnique.setFormalite(formalite);
             addOrUpdateFormaliteGuichetUnique(savedFormaliteGuichetUnique);
+            boolean isToSign = false;
+            boolean isToPay = false;
 
             if ((savedFormaliteGuichetUnique.getStatus().getCode()
                     .equals(FormaliteGuichetUniqueStatus.SIGNATURE_PENDING)
                     || savedFormaliteGuichetUnique.getStatus().getCode()
                             .equals(FormaliteGuichetUniqueStatus.AMENDMENT_SIGNATURE_PENDING)
                             && savedFormaliteGuichetUnique.getIsAuthorizedToSign() != null
-                            && savedFormaliteGuichetUnique.getIsAuthorizedToSign()))
+                            && savedFormaliteGuichetUnique.getIsAuthorizedToSign())) {
+                isToSign = true;
                 batchService.declareNewBatch(Batch.SIGN_FORMALITE_GUICHET_UNIQUE, savedFormaliteGuichetUnique.getId());
+            }
 
             if (formalite != null && savedFormaliteGuichetUnique != null && (Arrays
                     .asList(FormaliteGuichetUniqueStatus.PAYMENT_PENDING,
@@ -459,8 +463,17 @@ public class FormaliteGuichetUniqueServiceImpl implements FormaliteGuichetUnique
                             .asList(FormaliteGuichetUniqueStatus.AMENDMENT_PENDING))
                             .contains(savedFormaliteGuichetUnique.getStatus().getCode())
                             && savedFormaliteGuichetUnique.getIsAuthorizedToSign() != null
-                            && savedFormaliteGuichetUnique.getIsAuthorizedToSign())
+                            && savedFormaliteGuichetUnique.getIsAuthorizedToSign()) {
+                isToPay = true;
                 batchService.declareNewBatch(Batch.PAY_FORMALITE_GUICHET_UNIQUE, savedFormaliteGuichetUnique.getId());
+            }
+
+            if (savedFormaliteGuichetUnique != null && formalityHasNewStatus && !isToSign && !isToPay
+                    && savedFormaliteGuichetUnique.getIsAuthorizedToSign() != null
+                    && savedFormaliteGuichetUnique.getIsAuthorizedToSign()) {
+                savedFormaliteGuichetUnique.setIsAuthorizedToSign(false);
+                addOrUpdateFormaliteGuichetUnique(savedFormaliteGuichetUnique);
+            }
         }
         return savedFormaliteGuichetUnique;
 
