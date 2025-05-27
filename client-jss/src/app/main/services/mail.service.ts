f016f42586313bb5302179a31c53245ee131df6f
cocoJss <pcoanet@jss.fr>
2025-05-27 12:16:32 +0200

    new devs contribute component

---
+++ client-jss/src/app/main/services/mail.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { AppRestService } from "../../services/appRest.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MailService extends AppRestService<Boolean> {
+
+  constructor(http: HttpClient) {
+    super(http, "crm");
+  }
+
+  subscribeContactForm(mail: string, firstName: string, lastName: string, phone: string, message: string) {
+    let httpParams = new HttpParams().set("mail", mail).set("firstName", firstName).set("lastName", lastName).set("message", message);
+    if (phone && phone.length > 0)
+      httpParams = httpParams.set("phoneNumber", phone);
+    return this.get(httpParams, "subscribe/contact");
+  }
+}
