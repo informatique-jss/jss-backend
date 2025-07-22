5fe6fa241520106e084203647a137f813fc53b54
BOXEBELD Frédéric <fboxebeld@jss.fr>
2025-07-22 19:50:33 +0200

    first version of corrections of bugs with scroll, deleting unused spaces and correct display

--- client-myjss/src/app/modules/quotation/components/required-information/required-information.component.ts
+++ client-myjss/src/app/modules/quotation/components/required-information/required-information.component.ts
@@ -371,7 +371,7 @@ export class RequiredInformationComponent implements OnInit {
         }
 
         for (let provision of this.quotation.assoAffaireOrders[this.selectedAssoIndex].services[this.selectedServiceIndex].provisions)
-          if (provision && provision.announcement && !provision.isRedactedByJss && !this.isUsingTemplate && (!provision.announcement.notice || provision.announcement.notice.length == 0)) {
+          if (provision && provision.announcement && !provision.isRedactedByJss && !this.isUsingTemplate && (!provision.announcement || !provision.announcement.notice || provision.announcement.notice.length == 0)) {
             this.appService.displayToast("Veuillez remplir le texte de l'annonce légale", true, "Champs obligatoires", 5000);
             return of(false);
           }
@@ -424,6 +424,8 @@ export class RequiredInformationComponent implements OnInit {
         if (!response)
           return;
         this.quotationService.setCurrentDraftQuotationStep(this.appService.getAllQuotationMenuItems()[3]);
+        this.noticeTemplateDescription.isShowNoticeTemplate = false;
+        this.noticeTemplateService.changeNoticeTemplateDescription(this.noticeTemplateDescription);
         this.appService.openRoute(undefined, "quotation/checkout", undefined);
       });
       return;
