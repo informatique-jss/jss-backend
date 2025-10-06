bd07058dc5d5aca89d23244bd8bb16c82647e5e1
Alex Gapin <agapin@jss.fr>
2025-10-06 19:38:05 +0200

    fix

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   backendUrl: "https://server.osiris.jss.fr/",
   frontendUrl: "https://osiris.jss.fr/",
+  frontendUrlV2: "http://v2.osiris.jss.fr/",
   frontendMyJSSUrl: "https://my.jss.fr/"
 };
--- client/src/environments/environment.rec.ts
+++ client/src/environments/environment.rec.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   backendUrl: "http://rec-server.osiris.jss.fr/",
   frontendUrl: "http://rec.osiris.jss.fr/",
+  frontendUrlV2: "http://rec-v2.osiris.jss.fr/",
   frontendMyJSSUrl: "http://rec.my.jss.fr/"
 };
