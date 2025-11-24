22d5d452d7a7caba9c77296691083075096bc200
BOXEBELD Frédéric <fboxebeld@jss.fr>
2025-11-24 18:47:52 +0100

    fix PR review 2

--- client-myjss/src/app/libs/CoookieHelper.ts
+++
@@ -1,8 +0,0 @@
-// Even if consent is not given by the user, a cookie (anonyme) is created and will be found
-export function getGaClientId(): string {
-  const match = document.cookie.match('(?:^|;)\\s*_ga=([^;]*)');
-  // The format is often GA1.x.UID. We want to retrieve the UID part (from the 3rd segment)
-  // Or more simply, we take everything after "GA1.x."
-  const parts = match![1].split('.');
-  return parts.slice(2).join('.'); // returns something like "123456789.987654321"
-}
