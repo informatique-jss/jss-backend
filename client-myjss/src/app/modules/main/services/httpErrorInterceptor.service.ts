292b277f2a52040fa29f2b0606cce23b6dadda1c
BOXEBELD Frédéric <fboxebeld@jss.fr>
2025-11-25 11:45:40 +0100

    fix deployment of front

--- client-myjss/src/app/modules/main/services/httpErrorInterceptor.service.ts
+++ client-myjss/src/app/modules/main/services/httpErrorInterceptor.service.ts
@@ -89,11 +89,17 @@ export class HttpErrorInterceptor implements HttpInterceptor {
 
   // Even if consent is not given by the user, a cookie (anonyme) is created and will be found
   getGaClientId(): string {
-    const match = document.cookie.match('(?:^|;)\\s*_ga=([^;]*)');
-    // The format is often GA1.x.UID. We want to retrieve the UID part (from the 3rd segment)
-    // Or more simply, we take everything after "GA1.x."
-    const parts = match![1].split('.');
-    return parts.slice(2).join('.'); // returns something like "123456789.987654321"
+    if (this.platformService.getNativeDocument() != undefined) {
+      const match = this.platformService.getNativeDocument()!.cookie.match('(?:^|;)\\s*_ga=([^;]*)');
+      // The format is often GA1.x.UID. We want to retrieve the UID part (from the 3rd segment)
+      // Or more simply, we take everything after "GA1.x."
+      if (match && match[1]) {
+        const parts = match[1].split('.');
+        if (parts.length >= 3) {
+          return parts.slice(2).join('.'); // returns something like "123456789.987654321"
+        }
+      }
+    }
+    return ""; //  Fallback or error handling
   }
-
 }
