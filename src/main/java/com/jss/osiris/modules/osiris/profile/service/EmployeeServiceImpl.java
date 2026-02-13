7cc427f1e02de0933850fcdd94d11d8b66d1390a
Alex Gapin <agapin@jss.fr>
2026-02-13 14:53:28 +0100

    fix

--- src/main/java/com/jss/osiris/modules/osiris/profile/service/EmployeeServiceImpl.java
+++ src/main/java/com/jss/osiris/modules/osiris/profile/service/EmployeeServiceImpl.java
@@ -226,6 +226,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    @Transactional
     public void sendTokenToResponsable(Responsable responsable, String overrideMail) throws OsirisException {
         responsable = responsableService.getResponsable(responsable.getId());
 
