9faee7ec2808823e88926f01391c45aeca4aed04
Alex Gapin <agapin@jss.fr>
2026-02-20 14:09:43 +0100

    fix

--- client-myjss/src/app/libs/Constants.ts
+++ client-myjss/src/app/libs/Constants.ts
@@ -33,8 +33,10 @@ export const SERVICE_FIELD_TYPE_SELECT = "SERVICE_FIELD_TYPE_SELECT";
 
 export const MAX_SIZE_UPLOAD_FILES = 10485760;
 export const INVOICING_PAYMENT_LIMIT_REFUND_EUROS: number = 2;
+
 export const ASSO_SERVICE_DOCUMENT_ENTITY_TYPE: EntityType = { entityType: 'AssoServiceDocument', tabName: 'Documents du service', entryPoint: 'quotation/service' };
 export const APPLICATION_CV_ENTITY_TYPE: EntityType = { entityType: 'Candidacy', tabName: 'Candidature CV', entryPoint: 'application/cv' };
+export const PROVISION_ENTITY_TYPE: EntityType = { entityType: 'Provision', tabName: 'Prestation', entryPoint: 'provision/null' };
 
 export const JSS_IBAN = "FR76 3000 4007 9900 0257 1438 960"
 export const JSS_BIC = "BNPAFRPPXXX"
@@ -45,7 +47,6 @@ export const PROVISION_SCREEN_TYPE_ANNOUNCEMENT = "ANNOUNCEMENT";
 export const PROVISION_SCREEN_TYPE_FORMALITE = "FORMALITE";
 export const PROVISION_SCREEN_TYPE_STANDARD = "STANDARD";
 
-export const PROVISION_ENTITY_TYPE: EntityType = { entityType: 'Provision', tabName: 'Prestation', entryPoint: 'provision/null' };
 
 export const COOKIE_KEY = "consent-cookie";
 
