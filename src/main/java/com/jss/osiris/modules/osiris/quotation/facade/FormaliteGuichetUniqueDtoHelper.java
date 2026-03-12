1e63237fb36a1377a3d1ff8d51eea500f7dad78a
BOXEBELD Frédéric <fboxebeld@jss.fr>
2026-03-12 16:09:27 +0100

    fix bug on dates for GU formality

--- src/main/java/com/jss/osiris/modules/osiris/quotation/facade/FormaliteGuichetUniqueDtoHelper.java
+++ src/main/java/com/jss/osiris/modules/osiris/quotation/facade/FormaliteGuichetUniqueDtoHelper.java
@@ -12,68 +12,87 @@ import com.jss.osiris.modules.osiris.quotation.model.Service;
 import com.jss.osiris.modules.osiris.quotation.model.guichetUnique.FormaliteGuichetUnique;
 import com.jss.osiris.modules.osiris.quotation.model.guichetUnique.ValidationRequest;
 import com.jss.osiris.modules.osiris.quotation.model.guichetUnique.referentials.FormaliteGuichetUniqueStatus;
+import com.jss.osiris.modules.osiris.quotation.model.guichetUnique.referentials.FormaliteStatusHistoryItem;
 
 @org.springframework.stereotype.Service
 public class FormaliteGuichetUniqueDtoHelper {
 
-    public GuichetUniqueDepositInfoDto getCurrentGuInfoDto(FormaliteGuichetUnique formaliteGuichetUnique) {
-        GuichetUniqueDepositInfoDto currentGuInfoDto = new GuichetUniqueDepositInfoDto();
-        // Deposit date
-        currentGuInfoDto.setDepositDate(
-                OffsetDateTime.parse(formaliteGuichetUnique.getCreated()).toLocalDate());
+        public GuichetUniqueDepositInfoDto getCurrentGuInfoDto(FormaliteGuichetUnique formaliteGuichetUnique) {
+                GuichetUniqueDepositInfoDto currentGuInfoDto = new GuichetUniqueDepositInfoDto();
+                // Deposit date
+                currentGuInfoDto.setDepositDate(
+                                OffsetDateTime.parse(formaliteGuichetUnique.getCreated()).toLocalDate());
 
-        // Waiting for validation
-        if (formaliteGuichetUnique.getValidationsRequests() != null
-                && formaliteGuichetUnique.getValidationsRequests().size() > 0) {
-            LocalDate lastValidationRequestDate = formaliteGuichetUnique
-                    .getValidationsRequests().stream()
-                    .map(val -> OffsetDateTime.parse(val.getCreated()).toLocalDate())
-                    .max(Comparator.naturalOrder()).get();
+                // Waiting for validation
+                if (formaliteGuichetUnique.getValidationsRequests() != null
+                                && formaliteGuichetUnique.getValidationsRequests().size() > 0) {
+                        LocalDate lastValidationRequestDate = formaliteGuichetUnique
+                                        .getValidationsRequests().stream()
+                                        .map(val -> OffsetDateTime.parse(val.getCreated()).toLocalDate())
+                                        .max(Comparator.naturalOrder()).get();
 
-            currentGuInfoDto.setWaitingForValidationFromDate(lastValidationRequestDate);
+                        currentGuInfoDto.setWaitingForValidationFromDate(lastValidationRequestDate);
 
-            String partnerCenterName = "";
-            for (ValidationRequest validationRequest : formaliteGuichetUnique
-                    .getValidationsRequests()) {
-                if (validationRequest.getPartnerCenter() != null && !partnerCenterName
-                        .contains(validationRequest.getPartnerCenter().getName())) {
-                    partnerCenterName = partnerCenterName
-                            + validationRequest.getPartnerCenter().getName() + ", ";
+                        String partnerCenterName = "";
+                        for (ValidationRequest validationRequest : formaliteGuichetUnique
+                                        .getValidationsRequests()) {
+                                if (validationRequest.getPartnerCenter() != null && !partnerCenterName
+                                                .contains(validationRequest.getPartnerCenter().getName())) {
+                                        partnerCenterName = partnerCenterName
+                                                        + validationRequest.getPartnerCenter().getName() + ", ";
+                                }
+                        }
+                        currentGuInfoDto.setWaitingForValidationPartnerCenterName(partnerCenterName);
                 }
-            }
-            currentGuInfoDto.setWaitingForValidationPartnerCenterName(partnerCenterName);
+
+                // If validated
+                if (formaliteGuichetUnique.getStatus().getCode()
+                                .equals(FormaliteGuichetUniqueStatus.VALIDATED)) {
+                        if (formaliteGuichetUnique.getFormaliteStatusHistoryItems() != null) {
+                                for (FormaliteStatusHistoryItem formaliteStatusHistoryItem : formaliteGuichetUnique
+                                                .getFormaliteStatusHistoryItems()) {
+                                        if (FormaliteGuichetUniqueStatus.VALIDATED
+                                                        .equals(formaliteStatusHistoryItem.getStatus().getCode())) {
+                                                currentGuInfoDto.setValidationDate(OffsetDateTime
+                                                                .parse(formaliteStatusHistoryItem.getCreated())
+                                                                .toLocalDate());
+                                        }
+                                }
+                        } else {
+                                currentGuInfoDto
+                                                .setValidationDate(OffsetDateTime
+                                                                .parse(formaliteGuichetUnique.getStatusDate())
+                                                                .toLocalDate());
+                        }
+                }
+                return currentGuInfoDto;
         }
 
-        // If validated
-        if (formaliteGuichetUnique.getStatus().getCode()
-                .equals(FormaliteGuichetUniqueStatus.VALIDATED))
-            currentGuInfoDto
-                    .setValidationDate(OffsetDateTime.parse(formaliteGuichetUnique.getStatusDate())
-                            .toLocalDate());
-        return currentGuInfoDto;
-    }
+        public void addMissingAttachmentQueryToGuDepositInfoDtos(Service service,
+                        List<GuichetUniqueDepositInfoDto> guichetUniqueDepositInfoDtos) {
 
-    public void addMissingAttachmentQueryToGuDepositInfoDtos(Service service,
-            List<GuichetUniqueDepositInfoDto> guichetUniqueDepositInfoDtos) {
+                List<MissingAttachmentQuery> missingAttachmentsQueries = service.getMissingAttachmentQueries();
+                missingAttachmentsQueries = missingAttachmentsQueries.stream()
+                                .sorted((o1, o2) -> o1.getCreatedDateTime().compareTo(o2.getCreatedDateTime()))
+                                .toList();
 
-        List<MissingAttachmentQuery> missingAttachmentsQueries = service.getMissingAttachmentQueries();
-        missingAttachmentsQueries = missingAttachmentsQueries.stream()
-                .sorted((o1, o2) -> o1.getCreatedDateTime().compareTo(o2.getCreatedDateTime())).toList();
+                if (missingAttachmentsQueries != null && !missingAttachmentsQueries.isEmpty())
+                        for (GuichetUniqueDepositInfoDto guichetUniqueDepositInfoDto : guichetUniqueDepositInfoDtos) {
+                                List<LocalDate> askingMissingDocumentDates = new ArrayList<>();
+                                for (MissingAttachmentQuery missingAttachmentQuery : missingAttachmentsQueries)
+                                        if (missingAttachmentQuery.getCreatedDateTime().toLocalDate()
+                                                        .isAfter(guichetUniqueDepositInfoDto.getDepositDate())
+                                                        && missingAttachmentQuery.getCreatedDateTime().toLocalDate()
+                                                                        .isBefore(guichetUniqueDepositInfoDto
+                                                                                        .getValidationDate() != null
+                                                                                                        ? guichetUniqueDepositInfoDto
+                                                                                                                        .getValidationDate()
+                                                                                                        : LocalDate.MAX))
+                                                askingMissingDocumentDates.add(missingAttachmentQuery
+                                                                .getCreatedDateTime().toLocalDate());
 
-        if (missingAttachmentsQueries != null && !missingAttachmentsQueries.isEmpty())
-            for (GuichetUniqueDepositInfoDto guichetUniqueDepositInfoDto : guichetUniqueDepositInfoDtos) {
-                List<LocalDate> askingMissingDocumentDates = new ArrayList<>();
-                for (MissingAttachmentQuery missingAttachmentQuery : missingAttachmentsQueries)
-                    if (missingAttachmentQuery.getCreatedDateTime().toLocalDate()
-                            .isAfter(guichetUniqueDepositInfoDto.getDepositDate())
-                            && missingAttachmentQuery.getCreatedDateTime().toLocalDate()
-                                    .isBefore(guichetUniqueDepositInfoDto.getValidationDate() != null
-                                            ? guichetUniqueDepositInfoDto.getValidationDate()
-                                            : LocalDate.MAX))
-                        askingMissingDocumentDates.add(missingAttachmentQuery.getCreatedDateTime().toLocalDate());
-
-                guichetUniqueDepositInfoDto.setAskingMissingDocumentDates(askingMissingDocumentDates);
-            }
-    }
+                                guichetUniqueDepositInfoDto.setAskingMissingDocumentDates(askingMissingDocumentDates);
+                        }
+        }
 
 }
