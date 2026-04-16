890ae1e60618817ffcb8f3ef7c3d930c6e13cff4
mBennsir <mbennsir@jss.fr>
2026-04-16 15:26:52 +0200

    feature/OSI-9622 information banner update

--- client/src/app/modules/miscellaneous/model/InformationBanner.ts
+++ client/src/app/modules/miscellaneous/model/InformationBanner.ts
@@ -3,5 +3,4 @@ import { IReferential } from "../../administration/model/IReferential";
 export interface InformationBanner extends IReferential {
   isActive: boolean;
   text: string;
-  label: string;
 }
